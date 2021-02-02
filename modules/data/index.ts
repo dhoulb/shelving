a8f2a6248ecd438dcfd0bf917af6cb351352b67d
dhoulb <dave@shax.com>
2021-02-02 23:13:01 +0000

    fix: `Change` and `Result` etc apply specifically to `Data`

--- modules/data/index.ts
+++ modules/data/index.ts
@@ -1,4 +1,4 @@
-import type { DeepPartial, ImmutableObject } from "../object";
+import type { DeepPartial } from "../object";
 
 /**
  * Data: an object with string keys containing only plain values.
@@ -8,13 +8,13 @@ export type Data = { readonly [key: string]: Plain };
 export type Plain = number | string | boolean | null | readonly Plain[] | Data;
 
 /** Result: a result for document (i.e. the document's value or `undefined` if the document didn't exist. */
-export type Result<T extends ImmutableObject> = T | undefined;
+export type Result<T extends Data> = T | undefined;
 
 /** Results: a set of results for documents indexed by their ID (`undefined` isn't applicable because documents that don't exist won't appear in the results). */
-export type Results<T extends ImmutableObject> = { readonly [id: string]: T };
+export type Results<T extends Data> = { readonly [id: string]: T };
 
-/** Change: a change to some data (where `undefined` indicates deletion). */
-export type Change<T extends ImmutableObject> = T | DeepPartial<T>;
+/** Change: a change to a document's data (an explicit `undefined` for a prop indicates deletion). */
+export type Change<T extends Data> = T | DeepPartial<T>;
 
-/** Changes: a set of changes to multiple documents indexed by their ID. */
-export type Changes<T extends ImmutableObject> = { readonly [id: string]: Change<T> | undefined };
+/** Changes: a set of changes to multiple documents indexed by their ID (an explicit `undefined` for a document indicates deletion). */
+export type Changes<T extends Data> = { readonly [id: string]: Change<T> | undefined };
