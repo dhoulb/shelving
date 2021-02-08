ddf068fd429d0c9b79b647e8cbb610df71e1e3c1
dhoulb <dave@shax.com>
2021-02-08 15:27:16 +0000

    fix(react): Additional overloads for `useFetch()`
    - Allows generic types in dependencies to be carried through.

--- modules/react/useFetch.ts
+++ modules/react/useFetch.ts
@@ -10,13 +10,16 @@ import { SourceFetcher, useSource } from "./useSource";
  * @param deps Value the async value relies on like `useEffect()` and `useMemo()` etc. Deps are passed as the arguments to `fetch()` if it's a function.
  * @param maxAgeSeconds How 'out of date' data is allowed to be before it'll be refetched.
  */
-export const useFetch = <T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T => {
+export function useFetch<T, D extends Dependencies>(fetch: (...deps: D) => T | Promise<T>, deps: D, maxAgeSeconds?: number): T;
+export function useFetch<T, D extends Dependencies>(fetch: T, deps: D, maxAgeSeconds?: number): T;
+export function useFetch<T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T;
+export function useFetch<T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T {
 	const source = useSource<T>(`${fingerprint(fetch)}: ${fingerprint(deps)}`);
 	source.fetch(fetch, deps, maxAgeSeconds);
 	const { value, error } = source.value;
 	if (error) throw error;
 	return value;
-};
+}
 
 /**
  * Use the data of an async/promised value in a React component.
@@ -29,8 +32,11 @@ export const useFetch = <T, D extends Dependencies>(fetch: SourceFetcher<T, D>, 
  * @throws `RequiredError` when document does not exist.
  * @throws Unknown error when something goes wrong.
  */
-export const useFetchData = <T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T & ImmutableObject => {
+export function useFetchData<T, D extends Dependencies>(fetch: (...deps: D) => T | Promise<T>, deps: D, maxAgeSeconds?: number): T & ImmutableObject;
+export function useFetchData<T, D extends Dependencies>(fetch: T, deps: D, maxAgeSeconds?: number): T & ImmutableObject;
+export function useFetchData<T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T & ImmutableObject;
+export function useFetchData<T, D extends Dependencies>(fetch: SourceFetcher<T, D>, deps: D, maxAgeSeconds?: number): T & ImmutableObject {
 	const result = useFetch<T, D>(fetch, deps, maxAgeSeconds);
 	if (!isObject(result)) throw new RequiredError("useFetchData(): Fetched data was null or undefined");
 	return result;
-};
+}
