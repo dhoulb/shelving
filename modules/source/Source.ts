ea9295e7af12fe3f5db61fd66afeec4b6e21de6e
dhoulb <dave@shax.com>
2021-02-19 02:20:54 +0000

    fix(source): Fetching and subscribing defer to the end of the tick
    - So that we don't fetch something that's also being subscribed (wasteful!)

--- modules/source/Source.ts
+++ modules/source/Source.ts
@@ -1,20 +1,18 @@
 import type { Dependencies } from "../array";
+import { logError } from "../console";
+import { LOADING } from "../constants";
 import type { Subscriptor, Unsubscriber, AsyncFetcher } from "../function";
 import { State } from "../state";
 
 /** Default max age for data. */
-const MAX_AGE_SECONDS = 60;
+const MAX_AGE_MS = 60000;
 
 /** How long to wait to stop unused subscriptions. */
-const UNSUBSCRIBE_SECONDS = 10;
+const UNSUBSCRIBE_MS = 60000;
 
 /** Source is a type of State that can fetch or subscribe to a remote source. */
 export class Source<T> extends State<T> {
-	/** Date we last fetched the data. */
-	private _fetched?: number;
-
-	/** Function to call to stop the active subscription. */
-	private _unsubscribe?: Unsubscriber;
+	private _unsubscribe?: Unsubscriber; // Function to call to stop the active subscription.
 
 	/**
 	 * Derive a state that consumers who need realtime data can subscribe to.
@@ -22,64 +20,77 @@ export class Source<T> extends State<T> {
 	 */
 	readonly subscription: State<T> = this.derive();
 
-	/** Fetch this source's data from a data source. */
-	fetchFrom<D extends Dependencies>(fetcher: T | Promise<T> | AsyncFetcher<T, D>, deps: D, maxAgeSeconds = MAX_AGE_SECONDS): void {
-		// No need to fetch if the source is currently subscribed.
-		if (this._unsubscribe) return;
+	/**
+	 * Fetch this source's data from a data source.
+	 * @param fetcher The fetcher function to call to fetch the data.
+	 * @param deps Any dependencies the fetcher needs.
+	 * @param maxAge Skip the fetch if we already have a cached result that's younger than this (in milliseconds).
+	 */
+	fetchFrom<D extends Dependencies>(fetcher: AsyncFetcher<T, D>, deps: D, maxAge = MAX_AGE_MS): void {
+		// No need to subscribe if:
+		// 1. This source is closed.
+		// 2. A subscriber is already queued.
+		// 3. A fetcher is already queued.
+		// 4. The source is already subscribed.
+		// 5. We have a fetched result and it's younger than `maxAge`
+		if (
+			this.closed ||
+			this._queuedSubscribe ||
+			this.fetchFrom ||
+			this._unsubscribe ||
+			(typeof this.updated === "number" && Date.now() - this.updated < maxAge)
+		)
+			return;
 
-		// No need to fetch if result was fetched more recently than maxAgeSeconds
-		if (typeof this._fetched !== "number" || Date.now() - this._fetched < maxAgeSeconds * 1000) return;
+		// Queue to fetch at the end of the tick.
+		// Fetches and subscribes are deferred to the end of the tick so that we don't subscribe and fetch from the same source (which would be wasteful!)
+		this._queuedFetch = () => this.next(fetcher(...deps));
+		Promise.resolve().then(this._start, logError);
+	}
+	_queuedFetch?: () => void;
 
-		// Fetch.
+	/** Subcribe this source to a data source. */
+	subscribeTo<D extends Dependencies>(subscriptor: Subscriptor<T, D>, deps: D): void {
+		// No need to subscribe if:
+		// 1. This source is closed.
+		// 2. A subscriber is already queued.
+		// 3. The source is already subscribed.
+		if (this.closed || this._queuedSubscribe || this._unsubscribe) return;
+
+		// Queue to subscribe at the end of the tick.
+		// Fetches and subscribes are deferred to the end of the tick so that we don't subscribe and fetch from the same source (which would be wasteful!)
+		this._queuedSubscribe = () => subscriptor(this, ...deps);
+		Promise.resolve().then(this._start, logError);
+	}
+	_queuedSubscribe?: () => Unsubscriber;
+
+	// Run at the end of the tick to subscribe/fetch from the source.
+	// Fetches and subscribes are deferred to the end of the tick so that we don't subscribe and fetch from the same source (which would be wasteful!)
+	private _start = (): void => {
+		if (this.closed) return;
 		try {
-			const value = fetcher instanceof Function ? fetcher(...deps) : fetcher;
-			if (value instanceof Promise) {
-				void this._asyncNext(value);
-			} else {
-				this.next(value);
+			if (this._queuedSubscribe) {
+				// Subscribe beats fetch.
+				this._unsubscribe = this._queuedSubscribe();
+				this._cleanup(); // Unsubscribe again soon if no components subscribe.
+			} else if (this._queuedFetch) {
+				// Fetch if there's no subscription.
+				this._queuedFetch();
 			}
 		} catch (thrown) {
 			this.error(thrown);
 		}
-	}
-	private async _asyncNext(value: Promise<T>): Promise<void> {
-		try {
-			this.next(await value);
-		} catch (thrown) {
-			this.error(thrown);
-		}
-	}
-
-	/** Subcribe this source to a data source. */
-	subscribeTo<D extends Dependencies>(subscriptor: Subscriptor<T, D>, deps: D): void {
-		// No need to fetch if the source is currently subscribed.
-		if (this._unsubscribe) return;
-
-		// Subscribe.
-		try {
-			// Subscibe to the source now.
-			this._unsubscribe || subscriptor(this, ...deps);
-			// But unsubscribe again soon if no components subscribe.
-			this.cleanup();
-		} catch (thrown) {
-			this.error(thrown);
-		}
-	}
-
-	// Override `next()` to store the last fetched time.
-	next(value: T): void {
-		if (this.closed) return;
-		this._fetched = Date.now();
-		super.next(value);
-	}
+		this._queuedSubscribe = undefined;
+		this._queuedFetch = undefined;
+	};
 
 	/** Unsubscribe from the source soon if we have no subscribers. */
-	private cleanup() {
+	private _cleanup() {
 		if (this._timeout) clearTimeout(this._timeout);
 		if (this._unsubscribe && !this.subscription.subscribers) {
 			this._timeout = setTimeout(
 				() => this._unsubscribe && !this.subscription.subscribers && (this._unsubscribe = void this._unsubscribe()),
-				UNSUBSCRIBE_SECONDS * 1000,
+				UNSUBSCRIBE_MS,
 			);
 		}
 	}
@@ -87,4 +98,5 @@ export class Source<T> extends State<T> {
 }
 
 /** Create a new `Source` instance. */
-export const createSource = <T>(initialValue?: T): Source<T> => new Source<T>(initialValue);
+export const createSource = <T>(initial: Promise<T> | T): Source<T> => new Source<T>(initial);
+export const createLoadingSource = <T>(): Source<T> => new Source<T>(LOADING);
