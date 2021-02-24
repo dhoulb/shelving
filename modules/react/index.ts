2ad3003fe669a2dab97d9140ebe892e48b06c159
dhoulb <dave@shax.com>
2021-02-24 14:41:49 +0000

    fix(react): Missing export

--- modules/react/index.ts
+++ modules/react/index.ts
@@ -3,4 +3,5 @@ export * from "./useDocument";
 export * from "./useLazy";
 export * from "./usePagination";
 export * from "./useSource";
+export * from "./useState";
 export * from "./useSubscribe";
