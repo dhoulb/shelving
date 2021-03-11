52e6cb93fd2ab5f6e59883b4604720de27187971
dhoulb <dave@shax.com>
2021-03-11 13:06:29 +0000

    fix(search): Let `rank()` work recursively on objects too

--- modules/search/rank.ts
+++ modules/search/rank.ts
@@ -1,3 +1,6 @@
+import { isArray } from "../array";
+import { isObject } from "../object";
+
 /**
  * Searcher: similar to a `Matcher` but returns a relevance score (rather than discrete true/false)
  * - Consistent with: `Dispatcher`, `Deriver`, `Comparer`, `Matcher`
@@ -6,23 +9,23 @@ export type Ranker<L = unknown, R = unknown> = (item: L, target: R) => number;
 
 /**
  * Rank the relevance of an item to a target.
- * - Target is array: call `rank()` recursively for each target and return the total relevance.
- * - Item is array: call `rank()` recursively for each item and return the total relevance.
+ * - Target is array or object: call `rank()` recursively for each item/prop and sum the total relevance.
+ * - Item is array: call `rank()` recursively for each item/prop and sum the total relevance.
  * - Both strings: +1 if item includes target (i.e. for fulltext searching).
  * - Anything else: +1 if item and target are the same.
  */
 export const rank: Ranker = (item, target) => {
 	// Target is array: iterate through to calculate total score.
-	if (target instanceof Array) {
+	if (isArray(target) || isObject(target)) {
 		let relevance = 0;
-		for (const t of target) relevance += rank(item, t);
+		for (const t of Object.values(target)) relevance += rank(item, t);
 		return relevance;
 	}
 
 	// Item is array: iterate through to calculate total score.
-	if (item instanceof Array) {
+	if (isArray(item) || isObject(item)) {
 		let relevance = 0;
-		for (const i of item) relevance += rank(i, target);
+		for (const i of Object.values(item)) relevance += rank(i, target);
 		return relevance;
 	}
 
