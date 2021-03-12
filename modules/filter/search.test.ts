e0d239960bb8d92be8c279a85a32863fa84d7339
dhoulb <dave@shax.com>
2021-03-12 13:38:41 +0000

    fix(filter): Add `SEARCH.WORDS.ANY` matcher and rename `SEARCH.WORDS` to `SEARCH.WORDS.ALL`

--- modules/filter/search.test.ts
+++ modules/filter/search.test.ts
@@ -1,47 +1,102 @@
 import { SEARCH } from "..";
 
-test("SEARCH.WORDS: Works correctly", () => {
+test("SEARCH.WORDS.ALL: Works correctly", () => {
+	// Search empty query.
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "")).toBe(false);
+
 	// Single full words.
-	expect(SEARCH.WORDS("the dog and the cat", "dog")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "the")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "cat")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "tapir")).toBe(false);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "dog")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "the")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "tapir")).toBe(false);
 
 	// Multiple full words.
-	expect(SEARCH.WORDS("the dog and the cat", "dog cat")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "the the")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "tapir cat")).toBe(false);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "the the")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "tapir cat")).toBe(false);
 
 	// Single partial words.
-	expect(SEARCH.WORDS("the dog and the cat", "do")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "th")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "ca")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "tap")).toBe(false);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "do")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "th")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "ca")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "tap")).toBe(false);
 
 	// Multiple partial words.
-	expect(SEARCH.WORDS("the dog and the cat", "do ca")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "th t")).toBe(true);
-	expect(SEARCH.WORDS("the dog and the cat", "tap ca")).toBe(false);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "do ca")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "th t")).toBe(true);
+	expect(SEARCH.WORDS.ALL("the dog and the cat", "tap ca")).toBe(false);
 
 	// Arrays of words.
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "cat")).toBe(true);
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "man")).toBe(true);
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "the")).toBe(true);
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "tapir")).toBe(false);
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "dog cat")).toBe(true);
-	expect(SEARCH.WORDS(["the dog", "the man", "the cat"], "tapir cat")).toBe(false);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "man")).toBe(true);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "the")).toBe(true);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "tapir")).toBe(false);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL(["the dog", "the man", "the cat"], "tapir cat")).toBe(false);
 
 	// Objects of words.
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "cat")).toBe(true);
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "man")).toBe(true);
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "the")).toBe(true);
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "tapir")).toBe(false);
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "dog cat")).toBe(true);
-	expect(SEARCH.WORDS({ title: "the dog", type: "man", owns: "cat" }, "tapir cat")).toBe(false);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "man")).toBe(true);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "the")).toBe(true);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "tapir")).toBe(false);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ALL({ title: "the dog", type: "man", owns: "cat" }, "tapir cat")).toBe(false);
 
 	// Anything else.
-	expect(SEARCH.WORDS(123, "123")).toBe(false);
-	expect(SEARCH.WORDS(123, "123")).toBe(false);
-	expect(SEARCH.WORDS(true, "true")).toBe(false);
-	expect(SEARCH.WORDS(true, "true")).toBe(false);
+	expect(SEARCH.WORDS.ALL(123, "123")).toBe(false);
+	expect(SEARCH.WORDS.ALL(123, "123")).toBe(false);
+	expect(SEARCH.WORDS.ALL(true, "true")).toBe(false);
+	expect(SEARCH.WORDS.ALL(true, "true")).toBe(false);
+});
+test("SEARCH.WORDS.ANY: Works correctly", () => {
+	// Search empty query.
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "")).toBe(false);
+
+	// Single full words.
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "dog")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "the")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tapir")).toBe(false);
+
+	// Multiple full words.
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "the the")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tapir cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tapir elephant")).toBe(false);
+
+	// Single partial words.
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "do")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "th")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "ca")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tap")).toBe(false);
+
+	// Multiple partial words.
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "do ca")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "th t")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tap ca")).toBe(true);
+	expect(SEARCH.WORDS.ANY("the dog and the cat", "tap ele")).toBe(false);
+
+	// Arrays of words.
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "man")).toBe(true);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "the")).toBe(true);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "tapir")).toBe(false);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "tapir cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY(["the dog", "the man", "the cat"], "tapir elephant")).toBe(false);
+
+	// Objects of words.
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "man")).toBe(true);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "the")).toBe(true);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "tapir")).toBe(false);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "dog cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "tapir cat")).toBe(true);
+	expect(SEARCH.WORDS.ANY({ title: "the dog", type: "man", owns: "cat" }, "tapir elephant")).toBe(false);
+
+	// Anything else.
+	expect(SEARCH.WORDS.ANY(123, "123")).toBe(false);
+	expect(SEARCH.WORDS.ANY(123, "123")).toBe(false);
+	expect(SEARCH.WORDS.ANY(true, "true")).toBe(false);
+	expect(SEARCH.WORDS.ANY(true, "true")).toBe(false);
 });
--- modules/filter/search.ts
+++ modules/filter/search.ts
@@ -13,15 +13,20 @@ import { isObject } from "../object";
  * - Item is anything else: return false (can't be matched).
  * @param regexps The set of regular expressions to match against the item.
  */
-const searchRegExps: Matcher<unknown, ImmutableArray<RegExp>> = (item, regexps) => {
-	for (const regexp of regexps) if (!searchRegExp(item, regexp)) return false;
+const matchAll: Matcher<unknown, ImmutableArray<RegExp>> = (item, regexps) => {
+	if (!regexps.length) return false;
+	for (const regexp of regexps) if (!match(item, regexp)) return false;
 	return true;
 };
-const searchRegExp: Matcher<unknown, RegExp> = (item, regexp) => {
+const matchAny: Matcher<unknown, ImmutableArray<RegExp>> = (item, regexps) => {
+	for (const regexp of regexps) if (match(item, regexp)) return true;
+	return false;
+};
+const match: Matcher<unknown, RegExp> = (item, regexp) => {
 	// Item is array or object: recurse into it and return +1 if any item matches the regexp.
 	// e.g. `rank({ title: "Big Dog", description: "etc" }, "dog")`  will return `1`
 	if (isObject(item)) {
-		for (const i of Object.values(item)) if (searchRegExp(i, regexp)) return true;
+		for (const i of Object.values(item)) if (match(i, regexp)) return true;
 		return false;
 	}
 	// See if item is a string and matches the regexp.
@@ -30,24 +35,35 @@ const searchRegExp: Matcher<unknown, RegExp> = (item, regexp) => {
 
 /** Convert a string query to the corresponding set of case-insensitive regular expressions. */
 const prepWords = (query: string) => {
-	if (query !== cachedWordsQuery) {
-		cachedWordsQuery = query;
-		cachedWordsRegExps = toWords(query).map(toWordsRegExp);
+	if (query !== preppedWordsQuery) {
+		preppedWordsQuery = query;
+		preppedWordsRegExps = toWords(query).map(toWordsRegExp);
 	}
-	return cachedWordsRegExps;
+	return preppedWordsRegExps;
 };
-let cachedWordsQuery = "";
-let cachedWordsRegExps: ImmutableArray<RegExp> = [];
+let preppedWordsQuery = "";
+let preppedWordsRegExps: ImmutableArray<RegExp> = [];
 const toWordsRegExp = (word: string) => new RegExp(`\\b${escapeRegExp(word)}`, "i");
 
 /** Filter matchers that support fulltext searching. */
 export const SEARCH: {
-	WORDS: Matcher<unknown, string>;
+	WORDS: {
+		ANY: Matcher<unknown, string>;
+		ALL: Matcher<unknown, string>;
+	};
 } = {
-	/**
-	 * Fulltext search filter matcher that matches the start of every word in a query.
-	 * - Matches if every word in `query` matches the start of a word in `item`
-	 * - Case insensitive.
-	 */
-	WORDS: (item, query) => searchRegExps(item, prepWords(query)),
+	WORDS: {
+		/**
+		 * Fulltext search filter matcher that matches the start of **every** word in a query.
+		 * - Matches if **every** word in `query` matches the start of a word in `item`
+		 * - Case insensitive.
+		 */
+		ALL: (item, query) => matchAll(item, prepWords(query)),
+		/**
+		 * Fulltext search filter matcher that matches the start of **any** word in a query.
+		 * - Matches if **any** word in `query` matches the start of a word in `item`
+		 * - Case insensitive.
+		 */
+		ANY: (item, query) => matchAny(item, prepWords(query)),
+	},
 };
