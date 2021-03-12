8629b565ff0c8137acfaf9c18548b19f85ae3776
dhoulb <dave@shax.com>
2021-03-12 13:37:18 +0000

    test(filter): Add tests for `filter()`

---
+++ modules/filter/filter.test.ts
@@ -0,0 +1,49 @@
+import { filter, MATCH, SEARCH } from "..";
+
+test("filter(): Works correctly with MATCH", () => {
+	// Filters correctly.
+	expect(filter(["a", "b", "c"], MATCH.is, "b")).toEqual(["b"]);
+	expect(filter(["a", "b", "c"], MATCH.not, "b")).toEqual(["a", "c"]);
+	expect(filter(["a", "b", "c"], MATCH.in, ["c", "b"])).toEqual(["b", "c"]);
+	expect(filter([1, 2, 3], MATCH.gt, 2)).toEqual([3]);
+	expect(filter([1, 2, 3], MATCH.gte, 2)).toEqual([2, 3]);
+	expect(filter([1, 2, 3], MATCH.lte, 2)).toEqual([1, 2]);
+	expect(filter([1, 2, 3], MATCH.lt, 2)).toEqual([1]);
+	expect(filter(["a", "b", "c"], MATCH.gt, "b")).toEqual(["c"]);
+	expect(filter(["a", "b", "c"], MATCH.gte, "b")).toEqual(["b", "c"]);
+	expect(filter(["a", "b", "c"], MATCH.lte, "b")).toEqual(["a", "b"]);
+	expect(filter(["a", "b", "c"], MATCH.lt, "b")).toEqual(["a"]);
+	expect(filter([[1, 2, 3],  [4, 5, 6],  [6, 7, 8]], MATCH.contains, 5)).toEqual([[4, 5, 6]]); // prettier-ignore
+
+	// Returns same instance if no filtering needed.
+	const arr = ["a", "b", "c"];
+	expect(filter(arr, MATCH.in, ["c", "a", "b"])).toBe(arr);
+});
+test("filter(): Works correctly with SEARCH.WORDS.ALL", () => {
+	const arr = ["the dog", "the man", "the cat"];
+	expect(filter(arr, SEARCH.WORDS.ALL, "dog")).toEqual(["the dog"]);
+	expect(filter(arr, SEARCH.WORDS.ALL, "do")).toEqual(["the dog"]);
+	expect(filter(arr, SEARCH.WORDS.ALL, "man")).toEqual(["the man"]);
+	expect(filter(arr, SEARCH.WORDS.ALL, "ma")).toEqual(["the man"]);
+	expect(filter(arr, SEARCH.WORDS.ALL, "man cat")).toEqual([]);
+	expect(filter(arr, SEARCH.WORDS.ALL, "ma ca")).toEqual([]);
+
+	// Returs same instance if no filtering needed.
+	expect(filter(arr, SEARCH.WORDS.ALL, "the")).toBe(arr);
+	expect(filter(arr, SEARCH.WORDS.ALL, "th")).toBe(arr);
+});
+test("filter(): Works correctly with SEARCH.WORDS.ANY", () => {
+	const arr = ["the dog", "the man", "the cat"];
+	expect(filter(arr, SEARCH.WORDS.ANY, "dog")).toEqual(["the dog"]);
+	expect(filter(arr, SEARCH.WORDS.ANY, "do")).toEqual(["the dog"]);
+	expect(filter(arr, SEARCH.WORDS.ANY, "man")).toEqual(["the man"]);
+	expect(filter(arr, SEARCH.WORDS.ANY, "ma")).toEqual(["the man"]);
+	expect(filter(arr, SEARCH.WORDS.ANY, "man cat")).toEqual(["the man", "the cat"]);
+	expect(filter(arr, SEARCH.WORDS.ANY, "ma ca")).toEqual(["the man", "the cat"]);
+
+	// Returs same instance if no filtering needed.
+	expect(filter(arr, SEARCH.WORDS.ANY, "the")).toBe(arr);
+	expect(filter(arr, SEARCH.WORDS.ANY, "th")).toBe(arr);
+	expect(filter(arr, SEARCH.WORDS.ANY, "dog man cat")).toBe(arr);
+	expect(filter(arr, SEARCH.WORDS.ANY, "do ma ca")).toBe(arr);
+});
--- modules/sort/compare.test.ts
+++ modules/sort/compare.test.ts
@@ -1,55 +1,53 @@
 import { COMPARE } from "..";
 
-const { asc } = COMPARE;
-
 describe("compare()", () => {
 	test("Different types are sorted correctly", () => {
 		const arr = ["1", 1, true, 0, "0", "a", undefined, -1, false, null, {}];
-		arr.sort(asc);
+		arr.sort(COMPARE.asc);
 		expect(arr).toEqual([-1, 0, 1, "0", "1", "a", true, false, null, {}, undefined]);
 	});
 	test("Compare values of all types", () => {
 		// Number.
-		expect(asc(130, 125)).toBe(1);
-		expect(asc(125, 125)).toBe(0);
-		expect(asc(125, 130)).toBe(-1);
-		expect(asc(123, "abc")).toBe(-1);
+		expect(COMPARE.asc(130, 125)).toBe(1);
+		expect(COMPARE.asc(125, 125)).toBe(0);
+		expect(COMPARE.asc(125, 130)).toBe(-1);
+		expect(COMPARE.asc(123, "abc")).toBe(-1);
 		// Date.
 		// expect(compare(new Date(2), 1)).toBe(1);
 		// expect(compare(new Date(2), new Date(2))).toBe(0);
 		// expect(compare(new Date(), "abc")).toBe(-1);
 		// String.
-		expect(asc("abc", 123)).toBe(1);
-		expect(asc("abc", "abc")).toBe(0);
-		expect(asc("abc", true)).toBe(-1);
+		expect(COMPARE.asc("abc", 123)).toBe(1);
+		expect(COMPARE.asc("abc", "abc")).toBe(0);
+		expect(COMPARE.asc("abc", true)).toBe(-1);
 		// True.
-		expect(asc(true, "abc")).toBe(1);
-		expect(asc(true, true)).toBe(0);
-		expect(asc(true, false)).toBe(-1);
-		expect(asc(true, null)).toBe(-1);
-		expect(asc(true, NaN)).toBe(-1);
+		expect(COMPARE.asc(true, "abc")).toBe(1);
+		expect(COMPARE.asc(true, true)).toBe(0);
+		expect(COMPARE.asc(true, false)).toBe(-1);
+		expect(COMPARE.asc(true, null)).toBe(-1);
+		expect(COMPARE.asc(true, NaN)).toBe(-1);
 		// False.
-		expect(asc(false, true)).toBe(1);
-		expect(asc(false, false)).toBe(0);
-		expect(asc(false, null)).toBe(-1);
-		expect(asc(false, NaN)).toBe(-1);
+		expect(COMPARE.asc(false, true)).toBe(1);
+		expect(COMPARE.asc(false, false)).toBe(0);
+		expect(COMPARE.asc(false, null)).toBe(-1);
+		expect(COMPARE.asc(false, NaN)).toBe(-1);
 		// Null.
-		expect(asc(null, false)).toBe(1);
-		expect(asc(null, null)).toBe(0);
-		expect(asc(null, {})).toBe(-1);
-		expect(asc(null, NaN)).toBe(-1);
-		expect(asc(null, Symbol())).toBe(-1);
+		expect(COMPARE.asc(null, false)).toBe(1);
+		expect(COMPARE.asc(null, null)).toBe(0);
+		expect(COMPARE.asc(null, {})).toBe(-1);
+		expect(COMPARE.asc(null, NaN)).toBe(-1);
+		expect(COMPARE.asc(null, Symbol())).toBe(-1);
 		// Anything else.
-		expect(asc(NaN, null)).toBe(1);
-		expect(asc(NaN, NaN)).toBe(0);
-		expect(asc(NaN, undefined)).toBe(-1);
-		expect(asc(Symbol(), null)).toBe(1);
-		expect(asc(Symbol(), Symbol())).toBe(0);
-		expect(asc(Symbol(), undefined)).toBe(-1);
+		expect(COMPARE.asc(NaN, null)).toBe(1);
+		expect(COMPARE.asc(NaN, NaN)).toBe(0);
+		expect(COMPARE.asc(NaN, undefined)).toBe(-1);
+		expect(COMPARE.asc(Symbol(), null)).toBe(1);
+		expect(COMPARE.asc(Symbol(), Symbol())).toBe(0);
+		expect(COMPARE.asc(Symbol(), undefined)).toBe(-1);
 		// Undefined
-		expect(asc(undefined, Symbol())).toBe(1);
-		expect(asc(undefined, {})).toBe(1);
-		expect(asc(undefined, NaN)).toBe(1);
-		expect(asc(undefined, undefined)).toBe(0);
+		expect(COMPARE.asc(undefined, Symbol())).toBe(1);
+		expect(COMPARE.asc(undefined, {})).toBe(1);
+		expect(COMPARE.asc(undefined, NaN)).toBe(1);
+		expect(COMPARE.asc(undefined, undefined)).toBe(0);
 	});
 });
