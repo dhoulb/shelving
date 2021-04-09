44463a47917a69eba51447cc3928eaa5034a02a0
dhoulb <dave@shax.com>
2021-04-09 14:35:23 +0100

    refactor: Comments

--- modules/react/useSubscribe.ts
+++ modules/react/useSubscribe.ts
@@ -2,11 +2,13 @@ import { useEffect, useRef, useState as useReactState } from "react";
 import { Subscribable, Unsubscriber, LOADING, NOERROR } from "..";
 
 /**
- * Connect any `Subscribable` to a React component.
+ * Use the next value of a `Subscribable` object in a React component.
+ * - `Subscribable` objects are those that implement a `subcribe()` function that can be called with an `Observer` to receive a stream of values.
+ * - RxJS calls these `Observable`
  * - So the component refreshes when the subscribable issues a new value or errors.
  *
  * @returns The dispatched next value, or the `LOADING` symbol if no values have been dispatched yet.
- * @throws Any error caught by the subscribable.
+ * @throws Any error received by the subscribable.
  */
 export const useSubscribe = <T>(subscribable: Subscribable<T>): T | typeof LOADING => {
 	const [next, setNext] = useReactState<T | typeof LOADING>(LOADING);
