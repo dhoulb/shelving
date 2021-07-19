3e2b3a7df707a1c5dd04033e50a7550907c79c3b
dhoulb <dave@shax.com>
2021-07-19 09:06:18 +0100

    feat(react): Update `useDocument()` and `useDocuments()` to use update hooks and new `StateProvider`
    - To use `useDocument()` and `useDocuments(0` the underlying database must use a `StateProvider` instance

--- modules/react/useDocument.ts
+++ modules/react/useDocument.ts
@@ -1,15 +1,18 @@
-import { Data, Document, Result, State } from "..";
+import { assertInstance, Data, Document, Result, State, StateProvider, Unsubscriber } from "..";
+import { usePureEffect } from "./usePureEffect";
+import { usePureMemo } from "./usePureMemo";
 import { useState } from "./useState";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const UNDEFINED_STATE = new State<any>(undefined);
+const UNDEFINED_STATE = new State<Result<any>>(undefined);
 
 /**
  * Use a single document in a React component.
+ * - Requires database to use `StateProvider` and will error if this does not exist.
  *
  * @param ref Shelving `Document` reference, or an explicit `undefined`
  * - If `document` is `undefined` then `undefined` will always be returned.
- * @param maxAgeMs How 'out of date' data is allowed to be before it'll be refetched.
+ * @param maxAge How 'out of date' data is allowed to be before it'll be refetched.
  * - If `maxAge` is true, a realtime subscription to the data will be created.
  *
  * @returns `State` instance for the result of the document.
@@ -21,21 +24,24 @@ const UNDEFINED_STATE = new State<any>(undefined);
  *   - `state.reason` can tell you if the state has an error before you read `state.value`
  */
 export const useDocument = <T extends Data>(ref: Document<T> | undefined, maxAge: number | true = 1000): State<Result<T>> => {
-	const state: State<Result<T>> = ref ? ref.state : UNDEFINED_STATE;
-	if (ref && !state.closed) {
-		if (maxAge === true) {
-			// Start a source subscription on the state if there isn't one.
-			if (!state.started) {
-				// Start a subscription to the reference.
-				state.start(ref);
-				// If the state doesn't have any subscribers in a few seconds, stop the subscription again.
-				setTimeout(() => !state.subscribers && state.stop(), 5000);
-			}
-		} else {
-			// Get the next value from the ref if there's no subscription and the current one is too old.
-			if (!state.started && !state.pending && state.age < maxAge) state.next(ref.result);
-		}
+	let state: State<Result<T>> = UNDEFINED_STATE;
+
+	if (ref) {
+		const provider = ref.provider;
+		assertInstance(provider, StateProvider);
+		state = provider.getDocumentState<T>(ref);
+		provider.refreshDocumentState(ref, maxAge);
 	}
+
+	// Subscribe to the state so when it changes the component is rerendered.
 	useState(state);
+
+	// Keep any realtime subscriptions open for the lifespan of this component.
+	usePureEffect(subscribeEffect, [usePureMemo(ref, [ref?.path]), maxAge]);
+
 	return state;
 };
+
+/** Effect that keeps the realtime subscription (if `maxAge` is `true`) open for the lifespan of the component. */
+const subscribeEffect = <T extends Data>(ref: Document<T> | undefined, maxAge: number | true): Unsubscriber | void =>
+	ref && maxAge === true ? () => ref.subscribe({}) : undefined;
--- modules/react/useDocuments.ts
+++ modules/react/useDocuments.ts
@@ -1,17 +1,18 @@
-import { Data, Results, Documents } from "..";
-import { Source } from "./Source";
+import { Data, Results, Documents, assertInstance, State, StateProvider, Unsubscriber } from "..";
+import { usePureEffect } from "./usePureEffect";
+import { usePureMemo } from "./usePureMemo";
 import { useState } from "./useState";
 
-/** Store a list of named cached `Source` instances. */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const sources: { [key: string]: Source<any> } = {};
+const EMPTY_STATE = new State<Results<any>>({});
 
 /**
  * Use a set of documents in a React component.
+ * - Requires database to use `StateProvider` and will error if this does not exist.
  *
  * @param ref Shelving `Documents` reference, or an explicit `undefined`
  * - If `collection` is `undefined` then `{}` empty results will always be returned.
- * @param maxAgeMs How 'out of date' data is allowed to be before it'll be refetched.
+ * @param maxAge How 'out of date' data is allowed to be before it'll be refetched.
  * - If `maxAge` is true, a realtime subscription to the data will be created.
  *
  * @returns `State` instance for the results of the collection.
@@ -21,19 +22,25 @@ const sources: { [key: string]: Source<any> } = {};
  * - If the data results in an error, reading `state.value` will throw that error.
  *   - `state.reason` can tell you if the state has an error before you read `state.value`
  */
-export const useDocuments = <T extends Data>(
-	ref: Documents<T> | undefined,
-	options: { subscribe?: boolean; maxAge?: number; initial?: Results<T> },
-): Source<Results<T>> => {
-	const key = ref ? ref.toString() : "undefined";
-	const source: Source<Results<T>> = (sources[key] ||= new Source<Results<T>>(
-		!ref ? { initial: undefined } : { ...options, subscriptor: ref, fetcher: ref }, //
-	));
-	if (source.closed) setTimeout(() => source === sources[key] && delete sources[key], 3000);
+export const useDocuments = <T extends Data>(ref: Documents<T> | undefined, maxAge: number | true = 1000): State<Results<T>> => {
+	let state: State<Results<T>> = EMPTY_STATE;
+
 	if (ref) {
-		if (options.subscribe === true) source.start();
-		else source.queueFetch(options.maxAge);
+		const provider = ref.provider;
+		assertInstance(provider, StateProvider);
+		state = provider.getDocumentsState<T>(ref);
+		provider.refreshDocumentsState(ref, maxAge);
 	}
-	useState(source);
-	return source;
+
+	// Subscribe to the state so when it changes the component is rerendered.
+	useState(state);
+
+	// Keep any realtime subscriptions open for the lifespan of this component.
+	usePureEffect(subscribeEffect, [usePureMemo(ref, [ref?.path]), maxAge]);
+
+	return state;
 };
+
+/** Effect that keeps the realtime subscription (if `maxAge` is `true`) open for the lifespan of the component. */
+const subscribeEffect = <T extends Data>(ref: Documents<T> | undefined, maxAge: number | true): Unsubscriber | void =>
+	ref && maxAge === true ? () => ref.subscribe({}) : undefined;
