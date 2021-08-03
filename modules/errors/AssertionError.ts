41009712d24ae45caf6deb6cfa6fb928abb3782d
dhoulb <dave@shax.com>
2021-08-03 14:20:13 +0100

    refactor(errors): Comment consistency

--- modules/errors/AssertionError.ts
+++ modules/errors/AssertionError.ts
@@ -1,7 +1,7 @@
 import { debug } from "../util";
 
 /**
- * Thrown when the program receives a value it didn't expect.
+ * Thrown if the program receives a value it didn't expect.
  *
  * @param message Message that explains why the thing happened.
  * @param ...received One or more additional details that appears in a ` (received X, Y, Z)` part of the message.
