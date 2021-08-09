6bcf9f087e6686210453fdd158c9df21b12245f3
dhoulb <dave@shax.com>
2021-08-09 16:07:02 +0100

    feat(react): Add ability for `usePureMemo()` to accept a class constructor as its value

--- modules/react/usePureMemo.ts
+++ modules/react/usePureMemo.ts
@@ -3,29 +3,34 @@ import { Arguments, isArrayEqual, getLazy, Lazy } from "..";
 
 /**
  * Version of React's `useMemo()` that allows the use of a pure (side-effect free) function.
+ * - Also allows the first parameter to be a plain value that is memoised as long as `args` doesn't change.
+ * - Also allows the first parameter to be a class constructor that will create a new instance of that class.
  *
- * @param initialiser The value to memoise, or an initialiser function that returns that value.
- * - If this is a non-function, it is used as the memoised value (the same instance will be returned if `args` are unchanged).
- * - If this is a function, it is called to retrieve the memoised value with `args` as its parameters.
+ * @param value The value to memoise, or an initialiser function or class constructor that returns that value.
+ * - If this is a plain value, that value is returned.
+ * - If this is a function, it is called and its returned value is returned.
+ * - If this is a class constructor, a new instance of that class is instantiated and returned.
+ * - Class constructors are detected by whether the value is a function with a `name` property whose first character is an uppercase character.
  *
  * @param args Set of arguments that specify whether the returned value is refreshed or not.
- * - This array of values is passed _into_ the function as its parameters.
+ * - This array of values is passed into the function or class constructor as its parameters.
  * - This means you can create the function once (outside the component) rather than creating it on every render.
  * - This improves performance (though probably only noticeable on functions that render 1,000s of times).
  */
-export function usePureMemo<T, A extends Arguments>(initialiser: (...args: A) => T, args: A): T; // Generics flow through this overload better than using `Lazy`
-export function usePureMemo<T>(initialiser: T, args: Arguments): T;
-export function usePureMemo<T, A extends Arguments>(initialiser: Lazy<T, A>, args: A): T {
+export function usePureMemo<T, A extends Arguments>(value: (...args: A) => T, args: A): T; // Generics flow through this overload better than using `Lazy`
+export function usePureMemo<T, A extends Arguments>(value: new (...args: A) => T, args: A): T; // Generics flow through this overload better than using `Lazy`
+export function usePureMemo<T>(value: T, args: Arguments): T;
+export function usePureMemo<T, A extends Arguments>(value: Lazy<T, A>, args: A): T {
 	const internals = (useRef<{
 		value: T;
 		args: A;
 	}>().current ||= {
-		value: getLazy(initialiser, ...args),
+		value: getLazy(value, ...args),
 		args,
 	});
 
 	if (!isArrayEqual<A>(args, internals.args)) {
-		internals.value = getLazy(initialiser, ...args);
+		internals.value = getLazy(value, ...args);
 		internals.args = args;
 	}
 
