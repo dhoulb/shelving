a2fc396fabcf82e1c97cf0fbcb51772d32e9144f
dhoulb <dave@shax.com>
2021-10-12 17:57:37 +0100

    feat(react): Add `useMapState()` and `useArrayState()` functions

--- modules/react/useState.ts
+++ modules/react/useState.ts
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { LOADING, State } from "..";
+import { ArrayState, ImmutableArray, ImmutableObject, LOADING, MapState, State } from "..";
 import { useObserve } from "./useObserve";
 
 /**
@@ -17,6 +17,36 @@ export const useState = <T>(initial: State<T> | T | Promise<T> | typeof LOADING)
 
 	// Select either the `State` instance from the input parameters (if there is one) or use the memoized `State` instance from the initial value.
 	const whichState = initial instanceof State ? initial : memoizedState;
+
+	useObserve(whichState);
+	return whichState;
+};
+
+/**
+ * Subscribe to or create a new Shelving `MapState` instance.
+ * - Defaults to a new `MapState` instance with no entries.
+ */
+export const useMapState = <T>(initial?: MapState<T> | ImmutableObject<T> | Promise<ImmutableObject<T>> | typeof LOADING): MapState<T> => {
+	// Create a memoized `MapState` instance from the initial value (if it's not a state itself).
+	const memoizedState = (useRef<MapState<T>>().current ||= initial instanceof MapState ? initial : MapState.create<T>(initial));
+
+	// Select either the `MapState` instance from the input parameters (if there is one) or use the memoized `MapState` instance from the initial value.
+	const whichState = initial instanceof MapState ? initial : memoizedState;
+
+	useObserve(whichState);
+	return whichState;
+};
+
+/**
+ * Subscribe to or create a new Shelving `ArrayState` instance.
+ * - Defaults to a new `ArrayState` instance with no items.
+ */
+export const useArrayState = <T>(initial?: ArrayState<T> | ImmutableArray<T> | Promise<ImmutableArray<T>> | typeof LOADING): ArrayState<T> => {
+	// Create a memoized `ArrayState` instance from the initial value (if it's not a state itself).
+	const memoizedState = (useRef<ArrayState<T>>().current ||= initial instanceof ArrayState ? initial : ArrayState.create<T>(initial));
+
+	// Select either the `ArrayState` instance from the input parameters (if there is one) or use the memoized `ArrayState` instance from the initial value.
+	const whichState = initial instanceof ArrayState ? initial : memoizedState;
 
 	useObserve(whichState);
 	return whichState;
