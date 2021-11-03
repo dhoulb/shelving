6f0d87ae0b7c216c1f2941cce91337d5f79c3a91
dhoulb <dave@shax.com>
2021-11-03 19:04:28 +0000

    fix(db): Correct exports in `db/index.js`

--- modules/db/index.ts
+++ modules/db/index.ts
@@ -3,17 +3,15 @@ export * from "./Database.js";
 export * from "./Reference.js";
 export * from "./Document.js";
 export * from "./Documents.js";
-export * from "./Pagination.js";
-
-// States.
-export * from "./DocumentState.js";
-export * from "./DocumentsState.js";
 
 // Providers.
 export * from "./Provider.js";
+export * from "./ThroughProvider.js";
+export * from "./BatchProvider.js";
 export * from "./MemoryProvider.js";
 export * from "./ValidationProvider.js";
-export * from "./StateProvider.js";
+export * from "./CacheProvider.js";
 
 // Other.
+export * from "./PaginationState.js";
 export * from "./errors.js";
