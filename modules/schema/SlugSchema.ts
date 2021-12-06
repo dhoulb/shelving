7da29b12f9df120e0c435689989cb23732be4f66
dhoulb <dave@shax.com>
2021-12-06 16:41:55 +0000

    feat(schema): Add `SlugSchema` class and `SLUG` and `OPTIONAL_SLUG` shortcuts

---
+++ modules/schema/SlugSchema.ts
@@ -0,0 +1,25 @@
+import { toSlug } from "../util/index.js";
+import { OPTIONAL } from "./OptionalSchema.js";
+import { StringSchema } from "./StringSchema.js";
+
+/**
+ * Define a valid slug, e.g. `this-is-a-slug`
+ *
+ * - Useful for URL components, usernames, etc.
+ * - Minimum slug length is 2 characters.
+ * - Maximum slug length is 64 characters.
+ */
+export class SlugSchema extends StringSchema {
+	override readonly multiline = false;
+	override readonly min = 2;
+	override readonly max = 32;
+	override sanitize(unsafeString: string): string {
+		return toSlug(unsafeString);
+	}
+}
+
+/** Valid slug, e.g. `this-is-a-slug` */
+export const SLUG = new SlugSchema({});
+
+/** Valid slug, e.g. `this-is-a-slug`, or `null` */
+export const OPTIONAL_SLUG = OPTIONAL(SLUG);
