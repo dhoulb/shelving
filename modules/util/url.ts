40f00fb700b0bfb097064599dcb570e6f13b1a1a
dhoulb <dave@shax.com>
2021-12-11 09:39:14 +0000

    fix(util): Create `getURL()` helper to go with `toURL()` (matches how `getDate()` and `toDate()` work)

--- modules/util/url.ts
+++ modules/util/url.ts
@@ -1,24 +1,29 @@
-import { getRequired } from "./data.js";
+import { AssertionError } from "../error/index.js";
 
-/** Just get important part of a URL, e.g. `http://shax.com/test?uid=129483` → `shax.com/test` */
-export const formatUrl = (url: string | URL): string => {
-	const { host, pathname } = getRequired(toURL(url));
-	return `${host}${pathname.length > 1 ? pathname : ""}`;
-};
+/** Things that can be converted to a URL instance. */
+export type PossibleURL = string | URL;
+export type PossibleOptionalURL = PossibleURL | null;
 
-/**
- * Convert a string to a URL instance or return `null` if we can't.
- * - Automatically prepend `https://` if there's no `:` anywhere.
- *
- * @param url Base
- * @param
- */
-export function toURL(url: string | URL, base: URL | string | undefined = typeof window === "object" ? window.location.href : undefined): URL | null {
+/** Convert a possible URL to a URL or return `null` if conversion fails. */
+export function toURL(url: PossibleURL, base: PossibleOptionalURL = typeof window === "object" ? window.location.href : null): URL | null {
 	if (url instanceof URL) return url;
 	if (!url) return null;
 	try {
-		return new URL(url, base);
+		return new URL(url, base || undefined);
 	} catch (e) {
 		return null;
 	}
 }
+
+/** Convert a possible URL to a URL but throw `AssertionError` if conversion fails. */
+export function getURL(input: PossibleURL, base?: PossibleOptionalURL): URL {
+	const url = toURL(input, base);
+	if (!url) throw new AssertionError("Invalid URL", input);
+	return url;
+}
+
+/** Just get important part of a URL, e.g. `http://shax.com/test?uid=129483` → `shax.com/test` */
+export const formatUrl = (url: PossibleURL): string => {
+	const { host, pathname } = getURL(url);
+	return `${host}${pathname.length > 1 ? pathname : ""}`;
+};
