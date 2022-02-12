cbd16c2a68b5923b7712ce06a52b2907ef92b019
dhoulb <dave@shax.com>
2022-02-12 17:49:37 +0000

    test(markup): Fix failing `MarkupNode` tests

--- modules/markup/types.test.ts
+++
@@ -1,8 +0,0 @@
-import type { MarkupElement, MarkupNode } from "../index.js";
-
-test("Markup: types: check our custom JSX types are compatible with React's types", () => {
-	const a1: MarkupElement = { type: "div", key: null, props: {} };
-	const a2: React.ReactElement = a1;
-	const b1: MarkupNode = { type: "div", key: null, props: {} };
-	const b2: React.ReactNode = a1;
-});
--- modules/util/jsx.test.ts
+++ modules/util/jsx.test.ts
@@ -1,5 +1,12 @@
 import { nodeToText, renderMarkup, yieldElements } from "../index.js";
+import type { JSXElement, JSXNode } from "../index.js";
 
+test("JSX: types: check our custom JSX types are compatible with React's types", () => {
+	const a1: JSXElement = { type: "div", key: null, props: {} };
+	const a2: React.ReactElement = a1;
+	const b1: JSXNode = { type: "div", key: null, props: {} };
+	const b2: React.ReactNode = a1;
+});
 describe("nodeToText()", () => {
 	test("Nodes can be converted to plain text", () => {
 		expect(nodeToText(renderMarkup("PARAGRAPH"))).toBe("PARAGRAPH");
