fea2461423922c47ae9e77165d29c2c7cc55ac3d
dhoulb <dave@shax.com>
2022-05-18 16:52:37 +0100

    fix(react): Overload for `useReduce()` was incorrectly returning `undefined`

--- modules/react/useReduce.ts
+++ modules/react/useReduce.ts
@@ -2,10 +2,8 @@ import { useRef } from "react";
 import { Arguments } from "../util/index.js";
 
 /** Use memoised value with reduction logic. */
-export function useReduce<T, A extends Arguments = []>(reduce: (previous: T | undefined, ...a: A) => T | undefined, ...args: A): T | undefined;
-export function useReduce<T, A extends Arguments = []>(reduce: (previous: T | undefined, ...a: A) => T, ...args: A): T;
 export function useReduce<T, A extends Arguments = []>(reduce: (previous: T | undefined, ...a: A) => T, ...args: A): T {
-	const r = useRef<T | undefined>();
+	const r = useRef<T>();
 	r.current = reduce(r.current, ...args);
 	return r.current;
 }
