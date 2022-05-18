33ef0418d86490514f30006b67209f888e91ce45
dhoulb <dave@shax.com>
2022-05-18 16:53:08 +0100

    refactor(react): Update `usePagination()` to use `useReduce()`

--- modules/react/usePagination.ts
+++ modules/react/usePagination.ts
@@ -1,19 +1,21 @@
-import { DatabaseQuery, Results, PaginationState, Data } from "../index.js";
+import { QueryReference, Results, PaginationState, Data, initialState, isSameReference } from "../index.js";
 import { useSubscribe } from "./useSubscribe.js";
-import { useLazy } from "./useLazy.js";
+import { useReduce } from "./useReduce.js";
 
 /**
  * Use a `Pagination` for a collection.
  * - Doesn't persist the state, so if the component or anything beneath it throws the currently paginated results will be lost.
  */
-export function usePagination<T extends Data>(ref: DatabaseQuery<T>, initial?: Results<T>): PaginationState<T> {
-	const pagination = useLazy(_createPagination, useLazy(ref, [ref.toString()]), initial);
+export function usePagination<T extends Data>(ref: QueryReference<T>, initial?: Results<T>): PaginationState<T> {
+	const pagination = useReduce(_getPagination, ref, initial);
 	useSubscribe(pagination);
 	return pagination;
 }
 
-function _createPagination<T extends Data>(ref: DatabaseQuery<T>, initial?: Results<T>) {
-	const pagination = new PaginationState(ref);
-	if (initial) pagination.next(initial);
-	return pagination;
+/** Get a `PaginationState` for a query reference. */
+function _getPagination<T extends Data>(previous: PaginationState<T> | undefined, ref: QueryReference<T>, initial?: Results<T>): PaginationState<T> {
+	// If there's a previous `PaginationState` reuse it as long as the ref hasn't change.
+	if (previous && isSameReference(previous.ref, ref)) return previous;
+	// Make and return a new `PaginationState`
+	return initial ? initialState(initial, new PaginationState(ref)) : new PaginationState(ref);
 }
