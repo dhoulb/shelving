038b4bc66681428f3e8d6fb2bb6c0fc27765e3fb
dhoulb <dave@shax.com>
2022-05-23 21:15:12 +0100

    fix(util): Add `getNotNull()` and `getNotNullish()` helpers

--- modules/util/null.ts
+++ modules/util/null.ts
@@ -14,6 +14,12 @@ export function assertNotNull<T>(v: T | null): asserts v is T {
 	if (v === null) throw new AssertionError("Must not be null", v);
 }
 
+/** Get the not-nullish version of value. */
+export function getNotNull<T>(v: T | null): T {
+	assertNotNull(v);
+	return v;
+}
+
 /** Nullish is `null` or `undefined` */
 export type Nullish<T> = T | null | undefined;
 
@@ -26,4 +32,10 @@ export const notNullish = <T>(v: Nullish<T>): v is T => v !== null && v !== unde
 /** Assert that a value is not nullish. */
 export function assertNotNullish<T>(v: Nullish<T>): asserts v is T {
 	if (v === null || v === undefined) throw new AssertionError("Must not be null or undefined", v);
+}
+
+/** Get the not-nullish version of value. */
+export function getNotNullish<T>(v: Nullish<T>): T {
+	assertNotNullish(v);
+	return v;
 }
