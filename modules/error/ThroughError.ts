b20d463e3f9da00642fe84717a478e1c67ecb414
dhoulb <dave@shax.com>
2022-06-17 10:56:32 +0100

    feat(error): Create `ThroughError` for wrapping a source error

---
+++ modules/error/ThroughError.ts
@@ -0,0 +1,15 @@
+import { debug } from "../util/debug.js";
+
+/**
+ * Thrown to wrap an error with another error.
+ * - Merges the message and stack of the previous message.
+ */
+export class ThroughError extends Error {
+	readonly cause: Error | unknown;
+	constructor(message: string, cause: Error | unknown) {
+		super(message);
+		this.cause = cause;
+		this.stack = `${this.stack}\nCause: ${cause instanceof Error ? cause.stack : debug(cause)}`;
+	}
+}
+ThroughError.prototype.name = "ThroughError";
