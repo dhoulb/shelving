bc752bd19df43f8cbae8aed8b8411a2044f07638
dhoulb <dave@shax.com>
2022-06-30 12:01:39 +0100

    feat(db): Remove `PaginationState` and `usePagination()`
    - Functionality for loading more is moved into `useQuery()`

--- modules/db/index.ts
+++ modules/db/index.ts
@@ -1,6 +1,5 @@
 export * from "./Database.js";
 export * from "./Reference.js";
 export * from "./Operation.js";
-export * from "./PaginationState.js";
 export * from "./util.js";
 export * from "./errors.js";
--- modules/react/index.ts
+++ modules/react/index.ts
@@ -9,4 +9,3 @@ export * from "./useSubscribe.js";
 // DB.
 export * from "./useDocument.js";
 export * from "./useQuery.js";
-export * from "./usePagination.js";
