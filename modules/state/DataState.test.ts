10c9271c5114b1381cc2afed8fd1f71437584561
dhoulb <dave@shax.com>
2022-07-05 13:51:14 +0100

    test(state): Fix broken tests for `DataState`

--- modules/state/DataState.test.ts
+++ modules/state/DataState.test.ts
@@ -42,7 +42,6 @@ test("OptionalDataState.prototype.data", () => {
 	const state = new OptionalDataState<T>(null);
 	expect(state).toBeInstanceOf(OptionalDataState);
 	expect(state.value).toEqual(null);
-	expect(state.result).toEqual(null);
 	expect(() => state.data).toThrow(RequiredError);
 	// Ons and onces.
 	const calls: OptionalData<T>[] = [];
@@ -50,22 +49,18 @@ test("OptionalDataState.prototype.data", () => {
 	// Set data value.
 	expect(state.next({ a: 1 })).toBe(undefined);
 	expect(state.value).toEqual({ a: 1 });
-	expect(state.result).toEqual({ a: 1 });
 	expect(state.data).toEqual({ a: 1 });
 	// Update data value.
 	expect(state.update({ a: INCREMENT })).toBe(undefined);
 	expect(state.value).toEqual({ a: 2 });
-	expect(state.result).toEqual({ a: 2 });
 	expect(state.data).toEqual({ a: 2 });
 	// Delete data value.
 	expect(state.delete()).toBe(undefined);
 	expect(state.value).toBe(null);
-	expect(state.result).toBe(null);
 	expect(() => state.data).toThrow(RequiredError);
 	// Set null value.
 	expect(state.next(null)).toBe(undefined);
 	expect(state.value).toBe(null);
-	expect(state.result).toBe(null);
 	expect(() => state.data).toThrow(RequiredError);
 	// Checks.
 	expect(calls).toEqual([null, { a: 1 }, { a: 2 }, null]);
