ee74bb9c5c7418a063e3c9eb164a9d6cd25ef2db
dhoulb <dave@shax.com>
2022-07-12 15:09:30 +0100

    refactor(provider): Improve console messages in `DebugProvider`

--- modules/provider/DebugProvider.ts
+++ modules/provider/DebugProvider.ts
@@ -12,145 +12,145 @@ import { ThroughProvider } from "./ThroughProvider.js";
 /** Provider that logs its operations to the console for debugging purposes. */
 export class DebugProvider extends ThroughProvider {
 	override getDocument<T extends Data>(ref: DocumentReference<T>): OptionalEntity<T> | PromiseLike<OptionalEntity<T>> {
-		console.log(`Get "${ref}"`);
+		console.log(`Get ${ref}:`);
 		try {
 			const result = super.getDocument(ref);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error getting "${ref}"`, reason);
+						console.error(`Error: Get ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error getting "${ref}"`, reason);
+			console.error(`Error: Get ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override subscribeDocument<T extends Data>(ref: DocumentReference<T>, observer: PartialObserver<OptionalEntity<T>>): Unsubscribe {
-		console.log(`Subscribe "${ref}"`);
+		console.log(`Subscribe ${ref}:`);
 		return super.subscribeDocument(ref, new DatabaseDebugObserver(ref, observer));
 	}
 	override addDocument<T extends Data>(ref: QueryReference<T>, data: T): string | PromiseLike<string> {
-		console.log(`Add to "${ref}"`, data);
+		console.log(`Add ${ref}:`, data);
 		try {
 			const result = super.addDocument(ref, data);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error adding to "${ref}"`, reason);
+						console.error(`Error: Add ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error adding to "${ref}"`, reason);
+			console.error(`Error: Add ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override setDocument<T extends Data>(ref: DocumentReference<T>, data: T): void | PromiseLike<void> {
-		console.log(`Set "${ref}"`, data);
+		console.log(`Set ${ref}:`, data);
 		try {
 			const result = super.setDocument(ref, data);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error setting "${ref}"`, reason);
+						console.error(`Error: Set ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error setting "${ref}"`, reason);
+			console.error(`Error: Set ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override updateDocument<T extends Data>(ref: DocumentReference<T>, update: DataUpdate<T>): void | PromiseLike<void> {
-		console.log(`Update "${ref}"`, update.updates);
+		console.log(`Update ${ref}:`, update.updates);
 		try {
 			const result = super.updateDocument(ref, update);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error updating "${ref}"`, reason);
+						console.error(`Error: Update ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error updating "${ref}"`, reason);
+			console.error(`Error: Update ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override deleteDocument<T extends Data>(ref: DocumentReference<T>): void | PromiseLike<void> {
-		console.log(`Delete "${ref}"`);
+		console.log(`Delete ${ref}:`);
 		try {
 			const result = super.deleteDocument(ref);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error deleting "${ref}"`, reason);
+						console.error(`Error: Delete ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error deleting "${ref}"`, reason);
+			console.error(`Error: Delete ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override getQuery<T extends Data>(ref: QueryReference<T>): Entities<T> | PromiseLike<Entities<T>> {
-		console.log(`Get "${ref}"`);
+		console.log(`Get ${ref}:`);
 		try {
 			const results = super.getQuery(ref);
 			return isAsync(results)
 				? results.then(undefined, reason => {
-						console.error(`Error getting "${ref}"`, reason);
+						console.error(`Error: Get ${ref}:`, reason);
 						throw reason;
 				  })
 				: results;
 		} catch (reason) {
-			console.error(`Error getting "${ref}"`, reason);
+			console.error(`Error: Get ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override subscribeQuery<T extends Data>(ref: QueryReference<T>, observer: PartialObserver<Entities<T>>): Unsubscribe {
-		console.log(`Subscribe "${ref}"`);
+		console.log(`Subscribe ${ref}:`);
 		return super.subscribeQuery(ref, new DatabaseDebugObserver(ref, observer));
 	}
 	override setQuery<T extends Data>(ref: QueryReference<T>, data: T): number | PromiseLike<number> {
-		console.log(`Set "${ref}"`, data);
+		console.log(`Set ${ref}:`, data);
 		try {
 			const result = super.setQuery(ref, data);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error setting "${ref}"`, reason);
+						console.error(`Error: Set ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error setting "${ref}"`, reason);
+			console.error(`Error: Set ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override updateQuery<T extends Data>(ref: QueryReference<T>, update: DataUpdate<T>): number | PromiseLike<number> {
-		console.log(`Update "${ref}"`, update.updates);
+		console.log(`Update ${ref}:`, update.updates);
 		try {
 			const result = super.updateQuery(ref, update);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error updating "${ref}"`, reason);
+						console.error(`Error: Update ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error updating "${ref}"`, reason);
+			console.error(`Error: Update ${ref}:`, reason);
 			throw reason;
 		}
 	}
 	override deleteQuery<T extends Data>(ref: QueryReference<T>): number | PromiseLike<number> {
-		console.log(`Delete "${ref}"`);
+		console.log(`Delete ${ref}:`);
 		try {
 			const result = super.deleteQuery(ref);
 			return isAsync(result)
 				? result.then(undefined, reason => {
-						console.error(`Error writing "${ref}"`, reason);
+						console.error(`Error: Delete ${ref}:`, reason);
 						throw reason;
 				  })
 				: result;
 		} catch (reason) {
-			console.error(`Error writing "${ref}"`, reason);
+			console.error(`Error: Delete ${ref}:`, reason);
 			throw reason;
 		}
 	}
