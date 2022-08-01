5255b5779b18e0f046e273a290821152d7f6c76a
dhoulb <dave@shax.com>
2022-08-01 19:03:21 +0100

    fix(markup): Additional overrides for `getBlockRegExp()` and `getLineRegExp()`

--- modules/markup/regexp.ts
+++ modules/markup/regexp.ts
@@ -1,5 +1,5 @@
 import type { Data } from "../util/data.js";
-import { getRegExpSource, PossibleRegExp } from "../util/regexp.js";
+import { getRegExpSource, NamedRegExp, NamedRegExpData, PossibleRegExp } from "../util/regexp.js";
 import type { MarkupOptions } from "./options.js";
 
 /** Subset of `NamedRegExpArray<T>` that are the only things we're required return from a `MarkupMatcher` function. */
@@ -17,11 +17,17 @@ export const BLOCK_START_REGEXP = /^\n*|\n+/; // Starts at start of a block (one
 export const BLOCK_END_REGEXP = /\n*$|\n\n+/; // End of a block (two or more linebreaks or end of string).
 
 /** Create regular expression that matches a block of content. */
+export function getBlockRegExp<T extends NamedRegExpData>(content: NamedRegExp<T>): NamedRegExp<T>; // Named regexp will pass the same named groups through.
+export function getBlockRegExp<T extends string>(content: `(?<${T}>${string})`): NamedRegExp<{ [K in T]: string }>; // One named group is easy to detect.
+export function getBlockRegExp(content?: PossibleRegExp, end?: PossibleRegExp, start?: PossibleRegExp): RegExp; // Anything else falls back to `RegExp` and named groups must be asserted.
 export function getBlockRegExp(content: PossibleRegExp = BLOCK_REGEXP, end: PossibleRegExp = BLOCK_END_REGEXP, start: PossibleRegExp = BLOCK_START_REGEXP): RegExp {
 	return new RegExp(`(?:${getRegExpSource(start)})(?:${getRegExpSource(content)})(?:${getRegExpSource(end)})`);
 }
 
 /** Create regular expression that matches a line of content. */
+export function getLineRegExp<T extends NamedRegExpData>(content: NamedRegExp<T>): NamedRegExp<T>; // Named regexp will pass the same named groups through.
+export function getLineRegExp<T extends string>(content: `(?<${T}>${string})`): NamedRegExp<{ [K in T]: string }>; // One named group is easy to detect.
+export function getLineRegExp(content?: PossibleRegExp, end?: PossibleRegExp, start?: PossibleRegExp): RegExp; // Anything else falls back to `RegExp` and named groups must be asserted.
 export function getLineRegExp(content: PossibleRegExp = LINE_REGEXP, end: PossibleRegExp = LINE_END_REGEXP, start: PossibleRegExp = LINE_START_REGEXP): RegExp {
 	return new RegExp(`(?:${getRegExpSource(start)})(?:${getRegExpSource(content)})(?:${getRegExpSource(end)})`);
 }
