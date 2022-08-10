f0f110dbff0120324a88584a556db48c92600163
dhoulb <dave@shax.com>
2022-08-10 21:03:13 +0100

    test(constraint): Fix reference to `Entity`

--- modules/constraint/FilterConstraint.test.ts
+++ modules/constraint/FilterConstraint.test.ts
@@ -1,4 +1,4 @@
-import { basics, basic1, basic2, basic3, basic4, basic5, basic6, basic7, basic8, basic9, expectUnorderedKeys, BasicEntity } from "../test/index.js";
+import { basics, basic1, basic2, basic3, basic4, basic5, basic6, basic7, basic8, basic9, expectUnorderedKeys, BasicItemData } from "../test/index.js";
 import { Data, FilterConstraint, getIDs } from "../index.js";
 
 test("Typescript", () => {
@@ -18,87 +18,87 @@ test("construct", () => {
 });
 test("IS", () => {
 	// Match plain.
-	expect(new FilterConstraint<BasicEntity>("str", "aaa").match(basic1)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("str", "aaa").match(basic2)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("id", "basic1").match(basic1)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("id", "basic1").match(basic2)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("str", "aaa").match(basic1)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("str", "aaa").match(basic2)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("id", "basic1").match(basic1)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("id", "basic1").match(basic2)).toBe(false);
 	// Filter plain.
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str", "aaa").transform(basics), ["basic1"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str", "NOPE").transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num", 300).transform(basics), ["basic3"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num", 999999).transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("group", "a").transform(basics), ["basic1", "basic2", "basic3"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("group", "b").transform(basics), ["basic4", "basic5", "basic6"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("group", "c").transform(basics), ["basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id", "basic5").transform(basics), ["basic5"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str", "aaa").transform(basics), ["basic1"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str", "NOPE").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num", 300).transform(basics), ["basic3"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num", 999999).transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("group", "a").transform(basics), ["basic1", "basic2", "basic3"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("group", "b").transform(basics), ["basic4", "basic5", "basic6"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("group", "c").transform(basics), ["basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id", "basic5").transform(basics), ["basic5"]);
 });
 test("IN", () => {
 	// Match array.
-	expect(new FilterConstraint<BasicEntity>("str", ["aaa"]).match(basic1)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("str", ["aaa"]).match(basic2)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("id", ["basic1"]).match(basic1)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("id", ["basic1"]).match(basic2)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("str", ["aaa"]).match(basic1)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("str", ["aaa"]).match(basic2)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("id", ["basic1"]).match(basic1)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("id", ["basic1"]).match(basic2)).toBe(false);
 });
 test("NOT", () => {
 	// Match plain.
-	expect(new FilterConstraint<BasicEntity>("!str", "aaa").match(basic1)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("!str", "aaa").match(basic2)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("!id", "basic1").match(basic1)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("!id", "basic1").match(basic2)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("!str", "aaa").match(basic1)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("!str", "aaa").match(basic2)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("!id", "basic1").match(basic1)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("!id", "basic1").match(basic2)).toBe(true);
 	// Filter plain.
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("!str", "aaa").transform(basics), ["basic2", "basic3", "basic4", "basic5", "basic6", "basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("!group", "a").transform(basics), ["basic4", "basic5", "basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("!str", "aaa").transform(basics), ["basic2", "basic3", "basic4", "basic5", "basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("!group", "a").transform(basics), ["basic4", "basic5", "basic6", "basic7", "basic8", "basic9"]);
 });
 test("OUT", () => {
 	// Match array.
-	expect(new FilterConstraint<BasicEntity>("!str", ["aaa"]).match(basic1)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("!str", ["aaa"]).match(basic2)).toBe(true);
-	expect(new FilterConstraint<BasicEntity>("!id", ["basic1"]).match(basic1)).toBe(false);
-	expect(new FilterConstraint<BasicEntity>("!id", ["basic1"]).match(basic2)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("!str", ["aaa"]).match(basic1)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("!str", ["aaa"]).match(basic2)).toBe(true);
+	expect(new FilterConstraint<BasicItemData>("!id", ["basic1"]).match(basic1)).toBe(false);
+	expect(new FilterConstraint<BasicItemData>("!id", ["basic1"]).match(basic2)).toBe(true);
 });
 test("CONTAINS", () => {
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("tags[]", "odd").transform(basics), ["basic1", "basic3", "basic5", "basic7", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("tags[]", "even").transform(basics), ["basic2", "basic4", "basic6", "basic8"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("tags[]", "prime").transform(basics), ["basic1", "basic2", "basic3", "basic5", "basic7"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("tags[]", "NOPE").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("tags[]", "odd").transform(basics), ["basic1", "basic3", "basic5", "basic7", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("tags[]", "even").transform(basics), ["basic2", "basic4", "basic6", "basic8"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("tags[]", "prime").transform(basics), ["basic1", "basic2", "basic3", "basic5", "basic7"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("tags[]", "NOPE").transform(basics), []);
 });
 test("LT", () => {
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<", 500).transform(basics), ["basic1", "basic2", "basic3", "basic4"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<", 0).transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<", 1000).transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<", "ddd").transform(basics), ["basic1", "basic2", "basic3"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<", "a").transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<", "zzz").transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id<", "basic3").transform(basics), ["basic1", "basic2"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id<", "zzzzzz").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<", 500).transform(basics), ["basic1", "basic2", "basic3", "basic4"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<", 0).transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<", 1000).transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<", "ddd").transform(basics), ["basic1", "basic2", "basic3"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<", "a").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<", "zzz").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id<", "basic3").transform(basics), ["basic1", "basic2"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id<", "zzzzzz").transform(basics), getIDs(basics));
 });
 test("LTE", () => {
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<=", 500).transform(basics), ["basic1", "basic2", "basic3", "basic4", "basic5"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<=", 0).transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num<=", 1000).transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<=", "ddd").transform(basics), ["basic1", "basic2", "basic3", "basic4"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<=", "a").transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str<=", "zzz").transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id<=", "basic3").transform(basics), ["basic1", "basic2", "basic3"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id<=", "zzzzzz").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<=", 500).transform(basics), ["basic1", "basic2", "basic3", "basic4", "basic5"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<=", 0).transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num<=", 1000).transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<=", "ddd").transform(basics), ["basic1", "basic2", "basic3", "basic4"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<=", "a").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str<=", "zzz").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id<=", "basic3").transform(basics), ["basic1", "basic2", "basic3"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id<=", "zzzzzz").transform(basics), getIDs(basics));
 });
 test("GT", () => {
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>", 500).transform(basics), ["basic6", "basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>", 1000).transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>", 0).transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>", "eee").transform(basics), ["basic6", "basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>", "kkk").transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>", "").transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id>", "basic7").transform(basics), ["basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id>", "").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>", 500).transform(basics), ["basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>", 1000).transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>", 0).transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>", "eee").transform(basics), ["basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>", "kkk").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>", "").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id>", "basic7").transform(basics), ["basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id>", "").transform(basics), getIDs(basics));
 });
 test("GTE", () => {
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>=", 500).transform(basics), ["basic5", "basic6", "basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>=", 1000).transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("num>=", 0).transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>=", "eee").transform(basics), ["basic5", "basic6", "basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>=", "kkk").transform(basics), []);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("str>=", "").transform(basics), getIDs(basics));
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id>=", "basic7").transform(basics), ["basic7", "basic8", "basic9"]);
-	expectUnorderedKeys(new FilterConstraint<BasicEntity>("id>=", "").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>=", 500).transform(basics), ["basic5", "basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>=", 1000).transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("num>=", 0).transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>=", "eee").transform(basics), ["basic5", "basic6", "basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>=", "kkk").transform(basics), []);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("str>=", "").transform(basics), getIDs(basics));
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id>=", "basic7").transform(basics), ["basic7", "basic8", "basic9"]);
+	expectUnorderedKeys(new FilterConstraint<BasicItemData>("id>=", "").transform(basics), getIDs(basics));
 });
