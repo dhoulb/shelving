2a90fbbac59ba1df2f085205e5eb4d4ce5b05081
dhoulb <dave@shax.com>
2022-08-11 02:12:15 +0100

    test(provider): Remove unused imports

--- modules/provider/MemoryProvider.test.ts
+++ modules/provider/MemoryProvider.test.ts
@@ -1,4 +1,4 @@
-import { basics, people, basic1, basic2, basic3, basic4, basic5, basic6, basic7, basic8, basic9, person1, person2, person3, person4, person5, expectOrderedKeys, expectUnorderedKeys, BasicItemData, TestCollections } from "../test/index.js";
+import { basics, people, basic1, basic2, basic3, basic4, basic6, basic7, basic8, basic9, person1, person2, person3, person4, person5, expectOrderedKeys, expectUnorderedKeys, BasicItemData, TestCollections } from "../test/index.js";
 import { MemoryProvider, OptionalData, ImmutableArray, Database } from "../index.js";
 
 test("MemoryProvider: set/get/delete documents", () => {
