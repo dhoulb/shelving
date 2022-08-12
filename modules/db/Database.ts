b606c8d23d5dfc24d5d7512c84ddc7be3f6fd6db
dhoulb <dave@shax.com>
2022-08-12 19:54:31 +0100

    fix(db): Remove `Pick<T, K>` statements in `Database`

--- modules/db/Database.ts
+++ modules/db/Database.ts
@@ -15,16 +15,16 @@ abstract class BaseDatabase<T extends Datas> {
 	abstract readonly provider: Provider<T> | AsyncProvider<T>;
 
 	/** Create a query on a collection in this database. */
-	abstract collection<K extends Key<T>>(collection: K): Collection<Pick<T, K>, K> | AsyncCollection<Pick<T, K>, K>;
+	abstract collection<K extends Key<T>>(collection: K): Collection<T, K> | AsyncCollection<T, K>;
 
 	/** Create a query on a collection in this database. */
-	abstract query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): Query<Pick<T, K>, K> | AsyncQuery<Pick<T, K>, K>;
+	abstract query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): Query<T, K> | AsyncQuery<T, K>;
 
 	/** Reference an item in a collection in this database. */
-	abstract item<K extends Key<T>>(collection: K, id: string): Item<Pick<T, K>, K> | AsyncItem<Pick<T, K>, K>;
+	abstract item<K extends Key<T>>(collection: K, id: string): Item<T, K> | AsyncItem<T, K>;
 
 	/** Run a set of changes on this database. */
-	abstract change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<Pick<T, K>, K>>>[]): ItemChanges<Pick<T, K>, K> | Promise<ItemChanges<Pick<T, K>, K>>;
+	abstract change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<T, K>>>[]): ItemChanges<T, K> | Promise<ItemChanges<T, K>>;
 }
 
 /** Database with a synchronous provider. */
@@ -33,16 +33,16 @@ export class Database<T extends Datas = Datas> implements BaseDatabase<T> {
 	constructor(provider: Provider<T>) {
 		this.provider = provider;
 	}
-	collection<K extends Key<T>>(collection: K): Collection<Pick<T, K>, K> {
-		return new Collection<Pick<T, K>, K>(this, collection);
+	collection<K extends Key<T>>(collection: K): Collection<T, K> {
+		return new Collection<T, K>(this, collection);
 	}
-	query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): Query<Pick<T, K>, K> {
-		return new Query<Pick<T, K>, K>(this, collection, filters, sorts, limit);
+	query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): Query<T, K> {
+		return new Query<T, K>(this, collection, filters, sorts, limit);
 	}
-	item<K extends Key<T>>(collection: K, id: string): Item<Pick<T, K>, K> {
-		return new Item<Pick<T, K>, K>(this, collection, id);
+	item<K extends Key<T>>(collection: K, id: string): Item<T, K> {
+		return new Item<T, K>(this, collection, id);
 	}
-	change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<Pick<T, K>, K>>>[]): ItemChanges<Pick<T, K>, K> {
+	change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<T, K>>>[]): ItemChanges<T, K> {
 		return changeProvider(this.provider, changes);
 	}
 }
@@ -53,16 +53,16 @@ export class AsyncDatabase<T extends Datas = Datas> implements BaseDatabase<T> {
 	constructor(provider: AsyncProvider<T>) {
 		this.provider = provider;
 	}
-	collection<K extends Key<T>>(collection: K): AsyncCollection<Pick<T, K>, K> {
-		return new AsyncCollection<Pick<T, K>, K>(this, collection);
+	collection<K extends Key<T>>(collection: K): AsyncCollection<T, K> {
+		return new AsyncCollection<T, K>(this, collection);
 	}
-	query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): AsyncQuery<Pick<T, K>, K> {
-		return new AsyncQuery<Pick<T, K>, K>(this, collection, filters, sorts, limit);
+	query<K extends Key<T>>(collection: K, filters?: FilterList<ItemData<T[K]>>, sorts?: SortList<ItemData<T[K]>>, limit?: number | null): AsyncQuery<T, K> {
+		return new AsyncQuery<T, K>(this, collection, filters, sorts, limit);
 	}
-	item<K extends Key<T>>(collection: K, id: string): AsyncItem<Pick<T, K>, K> {
-		return new AsyncItem<Pick<T, K>, K>(this, collection, id);
+	item<K extends Key<T>>(collection: K, id: string): AsyncItem<T, K> {
+		return new AsyncItem<T, K>(this, collection, id);
 	}
-	change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<Pick<T, K>, K>>>[]): Promise<ItemChanges<Pick<T, K>, K>> {
+	change<K extends Key<T>>(...changes: DeepIterable<Nullish<WriteChange<T, K>>>[]): Promise<ItemChanges<T, K>> {
 		return changeAsyncProvider(this.provider, changes);
 	}
 }
