c9012eb8f2bdec94e34b7a36cf717df401082129
dhoulb <dave@shax.com>
2023-03-25 17:12:21 +0000

    feat(react): Allow `useState()` to subscribe to multiple states
    - Rest argument lets multiple `State` instances be passed in.
    - If more than one `State` instances are passed in then an array is returned with the same states in the same order.

--- modules/react/useState.ts
+++ modules/react/useState.ts
@@ -1,22 +1,33 @@
-import { useEffect, useState as useReactState } from "react";
-import { AnyState, State } from "../state/State.js";
-import { BLACKHOLE } from "../util/function.js";
-import { runSequence } from "../util/sequence.js";
+import { Dispatch, useEffect, useState as useReactState } from "react";
+import type { AnyState } from "../state/State.js";
+import { Handler, Stop, dispatch } from "../util/function.js";
+import { ImmutableArray } from "../util/array.js";
+import { mapArray } from "../util/transform.js";
+import { isDefined } from "../util/undefined.js";
 
 /**
- * Subscribe to a Shelving `State` instance.
+ * Subscribe to one or more Shelving `State` instances.
  *
  * @param state Shelving `State` instance.
  * - Subscription is recreated every time this value changes.
  * - Memoise this value to persist the subscription for the lifetime of the component.
  * - If the value is a `State` instance
  */
-export function useState<S extends AnyState>(state: S): S;
-export function useState<S extends AnyState>(state?: S | undefined): S | undefined;
-export function useState<S extends AnyState>(state?: S | undefined): S | undefined {
-	const setState = useReactState<unknown>(state && !state.loading ? state.value : State.NOVALUE)[1];
+export function useState<T extends AnyState>(state: T): T;
+export function useState<T extends AnyState>(state?: T | undefined): T | undefined;
+export function useState<T extends ImmutableArray<AnyState | undefined>>(...states: T): T;
+export function useState(...states: (AnyState | undefined)[]): ImmutableArray<AnyState | undefined> | AnyState | undefined {
+	const setValue = useReactState<unknown>(undefined)[1];
 	const [error, setError] = useReactState<Error | unknown>(undefined);
-	useEffect(state ? () => runSequence(state.next, setState, setError) : BLACKHOLE, [state]);
+	useEffect(() => {
+		const stops = mapArray(states, _startState, setValue, setError);
+		return () => stops.filter(isDefined).forEach(dispatch);
+	}, states);
 	if (error) throw error;
-	return state;
+	return states.length <= 1 ? states[0] : states;
+}
+
+/** Start a subscription to a `ReferenceState` instance. */
+function _startState(state: AnyState | undefined, setValue: Dispatch<[unknown]>, setError: Handler): Stop | undefined {
+	return state?.next.to(setValue, setError);
 }
