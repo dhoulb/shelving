927f9e6fe713757fb7694d9b8788c541d06cf15a
dhoulb <dave@shax.com>
2023-04-27 19:12:57 +0100

    test(util/data): Improve type tests for data utilities

--- modules/util/data.test.ts
+++ modules/util/data.test.ts
@@ -1,22 +1,55 @@
 import type { DataProp, FlatData, FlatDataProp } from "../index.js";
-import { Data } from "../index.js";
 
-test("Typescript", () => {
-	type T = {
-		readonly a: {
-			readonly a1: number;
-			readonly a2: { readonly a2a: boolean };
+type T = {
+	readonly a: {
+		readonly a1: number;
+		readonly a2: {
+			readonly a2a: boolean;
 		};
-		readonly g: string;
+	};
+	readonly g: string;
+};
+
+test("FlatDataProp", () => {
+	const validFlatDataProp1: ["a.a1", number] | ["a.a2.a2a", boolean] | ["g", string] = undefined as unknown as FlatDataProp<T>;
+
+	const validFlatDataProp2: ["a", { readonly a1: number; readonly a2: { readonly a2a: boolean } }] | ["g", string] = undefined as unknown as DataProp<T>;
+});
+test("FlatData", () => {
+	const validFlatData: FlatData<T> = {
+		"a.a1": 123,
+		"a.a2.a2a": true,
+		"g": "abc",
 	};
 
-	const a: {
-		readonly "a.a1": number;
-		readonly "a.a2.a2a": boolean;
-		readonly "g": string;
-	} = undefined as unknown as FlatData<T>;
+	// @ts-expect-error g is missing
+	const invalidFlatData1: FlatData<T> = {
+		"a.a1": 123,
+		"a.a2.a2a": true,
+		// "g": "abc",
+	};
 
-	const b: ["a.a1", number] | ["a.a2.a2a", boolean] | ["g", string] = undefined as unknown as FlatDataProp<T>;
+	const invalidFlatData2: FlatData<T> = {
+		// @ts-expect-error "a.a1" is not string.
+		"a.a1": "ERR",
+		// @ts-expect-error "a.a2.a2a" is not boolean.
+		"a.a2.a2a": 123,
+		// @ts-expect-error "g" is not string.
+		"g": 123,
+	};
 
-	const d: ["a", { readonly a1: number; readonly a2: { readonly a2a: boolean } }] | ["g", string] = undefined as unknown as DataProp<T>;
+	const invalidFlatData3: Partial<FlatData<T>> = {
+		// @ts-expect-error "unknown" is not a known prop.
+		unknown: "abc",
+	};
+
+	const invalidFlatData4: Partial<FlatData<T>> = {
+		// @ts-expect-error "a.unknown" is not a known prop.
+		"a.unknown": 123,
+	};
+
+	const invalidFlatData5: Partial<FlatData<T>> = {
+		// @ts-expect-error "a.a2.unknown" is not a known prop.
+		"a.a2.unknown": true,
+	};
 });
