84afd5ba8eb8913a3419c051073e358d7959a688
dhoulb <dave@shax.com>
2023-04-30 20:20:11 +0100

    fix(util/update): Improvements to `getUpdates()` and `Update`

--- modules/util/update.ts
+++ modules/util/update.ts
@@ -1,5 +1,5 @@
 import type { ImmutableArray } from "./array.js";
-import type { Data, FlatData, FlatDataKey } from "./data.js";
+import type { Data, DataProp, FlatData, FlatDataKey } from "./data.js";
 import { AssertionError } from "../error/AssertionError.js";
 import { reduceItems } from "./iterate.js";
 import { getNumber } from "./number.js";
@@ -7,56 +7,43 @@ import { getProps, isObject } from "./object.js";
 
 /** Set of named updates for a data object. */
 export type Updates<T extends Data = Data> = {
-	[K in FlatDataKey<T> as `${K}`]?: FlatData<T>[K]; // Set update.
+	[K in FlatDataKey<T> as K | `${K}`]?: FlatData<T>[K]; // Set update.
 } & {
 	[K in FlatDataKey<T> as `${K}+=` | `${K}-=`]?: FlatData<T>[K] extends number ? FlatData<T>[K] : never; // Increment update.
 };
 
 /** A single update to a keyed property in an object. */
 export type Update =
-	| {
-			type: "set";
-			keys: ImmutableArray<string>;
-			value: unknown;
-	  }
-	| {
-			type: "sum";
-			keys: ImmutableArray<string>;
-			value: number;
-	  };
+	| { action: "set"; keys: ImmutableArray<string>; value: unknown } //
+	| { action: "sum"; keys: ImmutableArray<string>; value: number };
 
 /** Yield the prop updates in an `Updates` object as a set of `Update` objects. */
-export function* getUpdates<T extends Data>(data: Updates<T>): Iterable<Update> {
-	for (const [keys, value] of getProps<Data>(data)) {
-		if (keys.endsWith("+=")) {
-			yield { type: "sum", keys: keys.slice(0, -2).split("."), value: getNumber(value) };
-		} else if (keys.endsWith("-=")) {
-			yield { type: "sum", keys: keys.slice(0, -2).split("."), value: 0 - getNumber(value) };
-		} else {
-			yield { type: "set", keys: keys.split("."), value };
-		}
-	}
+export function getUpdates<T extends Data>(data: Updates<T>): ImmutableArray<Update> {
+	return getProps(data).map(_getUpdate);
+}
+function _getUpdate([keys, value]: DataProp<Data>): Update {
+	if (keys.endsWith("+=")) return { action: "sum", keys: keys.slice(0, -2).split("."), value: getNumber(value) };
+	else if (keys.endsWith("-=")) return { action: "sum", keys: keys.slice(0, -2).split("."), value: 0 - getNumber(value) };
+	else return { action: "set", keys: keys.split("."), value };
 }
 
 /** Update a data object with a set of updates. */
 export function updateData<T extends Data>(data: T, updates: Updates<T>): T {
-	return reduceItems(getUpdates(updates), updateProp, data);
+	return reduceItems(getUpdates(updates), _updateProp, data);
 }
-
-/** Update a prop with an `Update` object. */
-export function updateProp<T extends Data>(obj: T, update: Update, i = 0): T {
-	const { keys, type, value } = update;
+function _updateProp<T extends Data>(obj: T, update: Update, i = 0): T {
+	const { action, keys, value } = update;
 	const key = keys[i];
 	if (!key) return obj; // Shouldn't happen.
 	const oldValue = obj[key];
 	let newValue: unknown = oldValue;
 	if (i === keys.length - 1) {
-		if (type === "sum") newValue = typeof oldValue === "number" ? oldValue + value : value;
-		else if (type === "set") newValue = value;
-		else return type; // Never happens.
+		if (action === "sum") newValue = typeof oldValue === "number" ? oldValue + value : value;
+		else if (action === "set") newValue = value;
+		else return action; // Never happens.
 	} else {
 		if (!isObject(oldValue)) throw new AssertionError(`Prop "${keys.slice(0, i + 1).join(".")}" is not an object`);
-		newValue = updateProp(oldValue, update, i + 1);
+		newValue = _updateProp(oldValue, update, i + 1);
 	}
 	return oldValue === newValue ? obj : { ...obj, [key]: newValue };
 }
