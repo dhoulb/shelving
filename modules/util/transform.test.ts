2688373f9f874cb9d3ec618e60837ec6c106577f
dhoulb <dave@shax.com>
2023-04-30 14:38:34 +0100

    fix(util/transform): Simplify transform utilities by removing unused `Transformable` interface

--- modules/util/transform.test.ts
+++ modules/util/transform.test.ts
@@ -1,34 +1,21 @@
-import { mapArray, mapObject, transform, transformObject } from "../index.js";
+import { getUndefined, mapArray, mapObject, transformObject } from "../index.js";
 
-test("transform()", () => {
-	expect(transform(10, n => n * n)).toBe(100);
-	expect(transform(10, { transform: n => n * n })).toEqual(100);
-});
 test("mapArray()", () => {
 	const arr = [1, 2, 3, 4];
 
 	// Square each number.
 	expect(mapArray(arr, n => n * n)).toEqual([1, 4, 9, 16]);
-	expect(mapArray(arr, { transform: n => n * n })).toEqual([1, 4, 9, 16]);
-	// Use a flat value instead of a mapper function.
-	expect(mapArray(arr, null)).toEqual([null, null, null, null]);
-	expect(mapArray(arr, null)).not.toBe(arr);
 });
 test("mapObject()", () => {
 	const obj = { a: 1, b: 2, c: 3, d: 4 };
 
 	// Square each number (input is object).
 	expect(mapObject(obj, n => n * n)).toEqual({ a: 1, b: 4, c: 9, d: 16 });
-	expect(mapObject(obj, { transform: n => n * n })).toEqual({ a: 1, b: 4, c: 9, d: 16 });
 });
 test("transformObject()", () => {
 	const obj = { a: 10, b: 20 };
-	// Plain transforms.
-	expect(transformObject(obj, { a: 100 })).toEqual({ a: 100, b: 20 });
-	expect(transformObject(obj, { a: 10 })).toEqual({ a: 10, b: 20 });
-	// expect(transformData(obj, { a: 10 })).toBe(obj); // Returns same instance.
 	// Function transforms.
 	expect(transformObject(obj, { b: n => n * n })).toEqual({ a: 10, b: 400 });
 	// `undefined` deletes the element.
-	expect(transformObject(obj, { a: undefined, b: 600 })).toEqual({ b: 600 });
+	expect(transformObject(obj, { a: getUndefined })).toEqual({ b: 20 });
 });
--- modules/util/transform.ts
+++ modules/util/transform.ts
@@ -3,21 +3,7 @@ import type { ImmutableDictionary, PossibleDictionary } from "./dictionary.js";
 import type { Entry } from "./entry.js";
 import type { Arguments } from "./function.js";
 import type { ImmutableObject, MutableObject, ObjectValue } from "./object.js";
-import { isFunction } from "./function.js";
-import { getProps, isObject } from "./object.js";
-
-/** Object that transforms an input value into an output value with its `transform()` method. */
-export interface Transformable<I, O, A extends Arguments = []> {
-	transform(input: I, ...args: A): O;
-}
-
-/** Object that transforms an input value into an output value with its `transform()` method. */
-export interface AsyncTransformable<I, O, A extends Arguments = []> {
-	transform(input: I, ...args: A): O | PromiseLike<O>;
-}
-
-/** Is an unknown value a transformable. */
-export const isTransformable = <T extends Transformable<unknown, unknown, Arguments>>(value: T | unknown): value is T => isObject(value) && typeof value.transform === "function";
+import { getProps } from "./object.js";
 
 /** Function that can transform an input value into an output value. */
 export type Transform<I, O, A extends Arguments = []> = (input: I, ...args: A) => O;
@@ -25,84 +11,72 @@ export type Transform<I, O, A extends Arguments = []> = (input: I, ...args: A) =
 /** Function that can transform an input value into an output value. */
 export type AsyncTransform<I, O, A extends Arguments = []> = (input: I, ...args: A) => O | PromiseLike<O>;
 
-/** Something that can transform an input value into an output value (or a plain value). */
-export type Transformer<I, O, A extends Arguments = []> = Transformable<I, O, A> | Transform<I, O, A> | O;
+/** Set of named transforms for a data object (or `undefined` to skip the transform). */
+export type Transforms<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []> = {
+	readonly [K in keyof I]?: Transform<I[K], O[K], A>;
+};
 
-/** Something that can transform an input value into an output value (or a plain value). */
-export type AsyncTransformer<I, O, A extends Arguments = []> = AsyncTransformable<I, O, A> | AsyncTransform<I, O, A> | O;
-
-/** Set of named transformers for a data object (or `undefined` to skip the transform). */
-export type Transformers<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []> = { readonly [K in keyof I]?: Transformer<I[K], O[K], A> };
-
-/** Transform a value using a transformer. */
-export function transform<I, O, A extends Arguments = []>(input: I, transformer: (v: I) => O, ...args: A): O; // Helps `O` carry through functions that use generics.
-export function transform<I, O, A extends Arguments = []>(input: I, transformer: (v: I) => O | PromiseLike<O>, ...args: A): O | PromiseLike<O>; // Helps `O` carry through functions that use generics.
-export function transform<I, O, A extends Arguments = []>(input: I, transformer: Transformer<I, O, A>, ...args: A): O; // No promise returned with synchronous transformer.
-export function transform<I, O, A extends Arguments = []>(input: I, transformer: AsyncTransformer<I, O, A>, ...args: A): O; // No promise returned with asynchronous transformer.
-export function transform<I, O, A extends Arguments = []>(input: I, transformer: AsyncTransformer<I, O, A>, ...args: A): O | PromiseLike<O> {
-	return isFunction(transformer) ? transformer(input, ...args) : isTransformable(transformer) ? transformer.transform(input, ...args) : transformer;
+/** Modify a set of items using a transform. */
+export function mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transform: (v: I, ...args: A) => O, ...args: A): Iterable<O>; // Helps `I` and `O` carry through functions that use generics.
+export function mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transform: Transform<I, O, A>, ...args: A): Iterable<O>;
+export function* mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transform: Transform<I, O, A>, ...args: A): Iterable<O> {
+	for (const item of items) yield transform(item, ...args);
 }
 
-/** Modify a set of items using a transformer. */
-export function mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transformer: (v: I, ...args: A) => O, ...args: A): Iterable<O>; // Helps `I` and `O` carry through functions that use generics.
-export function mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transformer: Transformer<I, O, A>, ...args: A): Iterable<O>;
-export function* mapItems<I, O, A extends Arguments = []>(items: Iterable<I>, transformer: Transformer<I, O, A>, ...args: A): Iterable<O> {
-	for (const item of items) yield transform(item, transformer, ...args);
+/** Modify the items of an array using a transform. */
+export function mapArray<T extends ImmutableArray>(arr: T, transform: Transform<ArrayItem<T>, ArrayItem<T>>): T; // Passthrough for transforms that return the same type and remove nothing.
+export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transform: (v: I, ...args: A) => O, ...args: A): ImmutableArray<O>; // Helps `I` and `O` carry through functions that use generics.
+export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transform: Transform<I, O, A>, ...args: A): ImmutableArray<O>;
+export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transform: Transform<I, O, A>, ...args: A): ImmutableArray<O> {
+	return Array.from(mapItems(arr, transform, ...args));
 }
 
-/** Modify the items of an array using a transformer. */
-export function mapArray<T extends ImmutableArray>(arr: T, transformer: Transformer<ArrayItem<T>, ArrayItem<T>>): T; // Passthrough for transformers that return the same type and remove nothing.
-export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transformer: (v: I, ...args: A) => O, ...args: A): ImmutableArray<O>; // Helps `I` and `O` carry through functions that use generics.
-export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transformer: Transformer<I, O, A>, ...args: A): ImmutableArray<O>;
-export function mapArray<I, O, A extends Arguments = []>(arr: Iterable<I>, transformer: Transformer<I, O, A>, ...args: A): ImmutableArray<O> {
-	return Array.from(mapItems(arr, transformer, ...args));
+/** Modify the values of the props of an object using a transform. */
+export function mapObject<T extends ImmutableObject>(obj: T, transform: Transform<ObjectValue<T>, ObjectValue<T>>): T; // Passthrough for transforms that return the same type and remove nothing.
+export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transform: (v: ObjectValue<I>, ...args: A) => ObjectValue<O>, ...args: A): O; // Helps `I` and `O` carry through functions that use generics.
+export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transform: Transform<ObjectValue<I>, ObjectValue<O>, A>, ...args: A): O;
+export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transform: Transform<ObjectValue<I>, ObjectValue<I>, A>, ...args: A): O {
+	return Object.fromEntries(mapEntries(getProps(obj), transform, ...args)) as O;
 }
 
-/** Modify the values of the props of an object using a transformer. */
-export function mapObject<T extends ImmutableObject>(obj: T, transformer: Transformer<ObjectValue<T>, ObjectValue<T>>): T; // Passthrough for transformers that return the same type and remove nothing.
-export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transformer: (v: ObjectValue<I>, ...args: A) => ObjectValue<O>, ...args: A): O; // Helps `I` and `O` carry through functions that use generics.
-export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transformer: Transformer<ObjectValue<I>, ObjectValue<O>>, ...args: A): O;
-export function mapObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transformer: Transformer<unknown, unknown>, ...args: A): O {
-	return Object.fromEntries(mapEntries(getProps(obj), transformer, ...args)) as O;
-}
+/** Modify the values of a dictionary using a transform. */
+export const mapDictionary: <I, O, A extends Arguments = []>(dictionary: PossibleDictionary<I>, transform: Transform<I, O, A>, ...args: A) => ImmutableDictionary<O> = mapObject;
 
-/** Modify the values of a dictionary using a transformer. */
-export const mapDictionary: <I, O, A extends Arguments = []>(dictionary: PossibleDictionary<I>, transformer: Transformer<I, O, A>, ...args: A) => ImmutableDictionary<O> = mapObject;
-
-/** Modify the values of a set of entries using a transformer. */
-export function* mapEntries<K, I, O, A extends Arguments = []>(entries: Iterable<Entry<K, I>>, transformer: Transformer<I, O, A>, ...args: A): Iterable<Entry<K, O>> {
-	for (const [k, v] of entries) yield [k, transform(v, transformer, ...args)];
+/** Modify the values of a set of entries using a transform. */
+export function* mapEntries<K, I, O, A extends Arguments = []>(entries: Iterable<Entry<K, I>>, transform: Transform<I, O, A>, ...args: A): Iterable<Entry<K, O>> {
+	for (const [k, v] of entries) yield [k, transform(v, ...args)];
 }
 
 /**
- * Transform an object using a set of named transformers.
+ * Transform an object using a set of named transforms.
  *
  * @returns Transformed object (or same object if no changes were made).
  */
-export function transformObject<T extends ImmutableObject, A extends Arguments = []>(obj: T, transforms: Transformers<T, T | Partial<T>, A>, ...args: A): T;
-export function transformObject<T extends ImmutableObject, A extends Arguments = []>(obj: T | Partial<T>, transforms: Transformers<T, T | Partial<T>, A>, ...args: A): Partial<T>;
-export function transformObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transforms: Transformers<I, O | Partial<O>, A>, ...args: A): O;
-export function transformObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I | Partial<I>, transforms: Transformers<I, O | Partial<O>, A>, ...args: A): Partial<O>;
-export function transformObject<A extends Arguments = []>(input: ImmutableObject, transforms: Transformers<ImmutableObject, ImmutableObject, A>, ...args: A): ImmutableObject {
+export function transformObject<T extends ImmutableObject, A extends Arguments = []>(obj: T, transforms: Transforms<T, T | Partial<T>, A>, ...args: A): T;
+export function transformObject<T extends ImmutableObject, A extends Arguments = []>(obj: T | Partial<T>, transforms: Transforms<T, T | Partial<T>, A>, ...args: A): Partial<T>;
+export function transformObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I, transforms: Transforms<I, O | Partial<O>, A>, ...args: A): O;
+export function transformObject<I extends ImmutableObject, O extends ImmutableObject, A extends Arguments = []>(obj: I | Partial<I>, transforms: Transforms<I, O | Partial<O>, A>, ...args: A): Partial<O>;
+export function transformObject<A extends Arguments = []>(input: ImmutableObject, transforms: Transforms<ImmutableObject, ImmutableObject, A>, ...args: A): ImmutableObject {
 	let changed = false;
 	const output: MutableObject = { ...input };
 	for (const [k, t] of getProps(transforms)) {
-		// if (t !== undefined) {
-		const i = input[k];
-		const o = transform(i, t, ...args);
-		if (t === undefined) {
-			delete output[k];
-		} else {
-			output[k] = o;
-			if (!changed && i !== o) changed = true;
+		if (t) {
+			const i = input[k];
+			const o = t(i, ...args);
+			if (t === undefined) {
+				delete output[k];
+			} else {
+				output[k] = o;
+				if (!changed && i !== o) changed = true;
+			}
 		}
 	}
 	return changed ? output : input;
 }
 
-/** Transform items in a sequence as they are yielded using a (potentially async) transformer. */
-export function mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transformer: (input: I, ...args: A) => O | PromiseLike<O>, ...args: A): AsyncIterable<O>;
-export function mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transformer: AsyncTransformer<I, O, A>, ...args: A): AsyncIterable<O>;
-export async function* mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transformer: AsyncTransformer<I, O, A>, ...args: A): AsyncIterable<O> {
-	for await (const item of sequence) yield transform(item, transformer, ...args);
+/** Transform items in a sequence as they are yielded using a (potentially async) transform. */
+export function mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transform: (input: I, ...args: A) => O | PromiseLike<O>, ...args: A): AsyncIterable<O>;
+export function mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transform: AsyncTransform<I, O, A>, ...args: A): AsyncIterable<O>;
+export async function* mapSequence<I, O, A extends Arguments = []>(sequence: AsyncIterable<I>, transform: AsyncTransform<I, O, A>, ...args: A): AsyncIterable<O> {
+	for await (const item of sequence) yield transform(item, ...args);
 }
