a0646d8a05c374f7863c65402f3acbd1310ba44c
dhoulb <dave@shax.com>
2023-05-20 22:48:28 +0100

    fix(provider): Console output improvements to `DebugProvider`

--- modules/provider/DebugProvider.ts
+++ modules/provider/DebugProvider.ts
@@ -10,29 +10,27 @@ import type { Updates } from "../util/update.js";
 abstract class AbstractDebugProvider {
 	abstract readonly source: Provider | AsyncProvider;
 	async *getItemSequence(collection: string, id: string): AsyncIterableIterator<ItemValue> {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("ITERATE", key, "DATA:");
+			console.debug("✔ ITERATE", collection, id);
 			for await (const item of this.source.getItemSequence(collection, id)) {
-				console.log("ITERATE", key, "GOT:", item);
+				console.debug("✔ ITERATE", collection, id, "GOT", item);
 				yield item;
 			}
-			console.log("ITERATE", key, "DONE");
+			console.debug("✔ ITERATE", collection, id);
 		} catch (thrown) {
-			console.error("ITERATE", key, thrown);
+			console.error("✘ ITERATE", collection, id, thrown);
 		}
 	}
 	async *getQuerySequence(collection: string, query: ItemQuery): AsyncIterableIterator<ItemArray> {
-		const key = _getQueryKey(collection, query);
 		try {
-			console.log("ITERATE", key, "DATA:");
+			console.debug("✔ ITERATE", collection, query);
 			for await (const items of this.source.getQuerySequence(collection, query)) {
-				console.log("ITERATE", key, "ITEMS:", items);
+				console.debug("✔ ITERATE", collection, query, items);
 				yield items;
 			}
-			console.log("ITERATE", key, "DONE");
+			console.debug("✔ ITERATE", collection, query);
 		} catch (thrown) {
-			console.error("ITERATE", key, thrown);
+			console.error("✘ ITERATE", collection, query, thrown);
 		}
 	}
 }
@@ -45,98 +43,89 @@ export class DebugProvider extends AbstractDebugProvider implements ThroughProvi
 		this.source = source;
 	}
 	getItem(collection: string, id: string): ItemValue {
-		const key = _getItemKey(collection, id);
 		try {
 			const item = this.source.getItem(collection, id);
-			console.log("GET:", key, "ITEM:", item);
+			console.debug("✔ GET", collection, id, "ITEM", item);
 			return item;
 		} catch (reason) {
-			console.error("GET:", key, "ERROR:", reason);
+			console.error("✘ GET", collection, id, reason);
 			throw reason;
 		}
 	}
 	addItem(collection: string, data: Data): string {
-		const key = collection;
 		try {
 			const id = this.source.addItem(collection, data);
-			console.log("ADD", key, "DATA:", data, "ID", id);
+			console.debug("✔ ADD", collection, data, "ID", id);
 			return id;
 		} catch (reason) {
-			console.error("ADD", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ ADD", collection, data, reason);
 			throw reason;
 		}
 	}
 	setItem(collection: string, id: string, data: Data): void {
-		const key = _getItemKey(collection, id);
 		try {
 			this.source.setItem(collection, id, data);
-			console.log("SET:", key, "DATA:", data);
+			console.debug("✔ SET", collection, id, data);
 		} catch (reason) {
-			console.error("SET:", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ SET", collection, id, data, reason);
 			throw reason;
 		}
 	}
 	updateItem(collection: string, id: string, updates: Updates): void {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("UPDATE:", key, "UPDATES:", updates);
+			console.debug("✔ UPDATE", collection, id, updates);
 			return this.source.updateItem(collection, id, updates);
 		} catch (reason) {
-			console.error("UPDATE:", key, "UPDATES:", updates, "ERROR:", reason);
+			console.error("✘ UPDATE", collection, id, updates, reason);
 			throw reason;
 		}
 	}
 	deleteItem(collection: string, id: string): void {
-		const key = _getItemKey(collection, id);
 		try {
 			this.source.deleteItem(collection, id);
-			console.log("DELETE:", key);
+			console.debug("✔ DELETE", collection, id);
 		} catch (reason) {
-			console.error("DELETE:", key, "ERROR:", reason);
+			console.error("✘ DELETE", collection, id, reason);
 			throw reason;
 		}
 	}
 	getQuery(collection: string, query: ItemQuery): ItemArray {
-		const key = _getQueryKey(collection, query);
 		try {
 			const items = this.source.getQuery(collection, query);
-			console.log("GET:", key, "ITEMS:", items);
+			console.debug("✔ ✅ GET", collection, query, "ITEMS", items);
 			return items;
 		} catch (reason) {
-			console.error("GET:", key, "ERROR:", reason);
+			console.error("✘ GET", collection, query, reason);
 			throw reason;
 		}
 	}
 	setQuery(collection: string, query: ItemQuery, data: Data): number {
-		const key = _getQueryKey(collection, query);
 		try {
 			const num = this.source.setQuery(collection, query, data);
-			console.log("SET:", key, "DATA:", data, "DONE:", num);
+			console.debug("✔ SET", collection, query, data, num);
 			return num;
 		} catch (reason) {
-			console.error("SET:", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ SET", collection, query, data, reason);
 			throw reason;
 		}
 	}
 	updateQuery(collection: string, query: ItemQuery, updates: Updates): number {
-		const key = _getQueryKey(collection, query);
 		try {
 			const num = this.source.updateQuery(collection, query, updates);
-			console.log("UPDATE:", key, "UPDATES:", updates, "DONE:", num);
+			console.debug("✔ UPDATE", collection, query, updates, num);
 			return num;
 		} catch (reason) {
-			console.error("UPDATE:", key, "UPDATES:", updates, "ERROR:", reason);
+			console.error("✘ UPDATE", collection, query, updates, reason);
 			throw reason;
 		}
 	}
 	deleteQuery(collection: string, query: ItemQuery): number {
-		const key = _getQueryKey(collection, query);
 		try {
 			const num = this.source.deleteQuery(collection, query);
-			console.log("DELETE:", key, "DONE:", num);
+			console.debug("✔ DELETE", collection, query, num);
 			return num;
 		} catch (reason) {
-			console.error("DELETE:", key, "ERROR:", reason);
+			console.error("✘ DELETE", collection, query, reason);
 			throw reason;
 		}
 	}
@@ -150,111 +139,99 @@ export class AsyncDebugProvider extends AbstractDebugProvider implements AsyncTh
 		this.source = source;
 	}
 	async getItem(collection: string, id: string): Promise<ItemValue> {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("GET:", key);
+			console.debug("⋯ GET", collection, id);
 			const item = await this.source.getItem(collection, id);
-			console.log("GET:", key, "ITEM:", item);
+			console.debug("✔ GET", collection, id, "ITEM", item);
 			return item;
 		} catch (reason) {
-			console.error("GET:", key, "ERROR:", reason);
+			console.error("✘ GET", collection, id, reason);
 			throw reason;
 		}
 	}
 	async addItem(collection: string, data: Data): Promise<string> {
-		const key = collection;
 		try {
-			console.log("ADD", key, "DATA:", data);
+			console.debug("⋯ ADD", collection, data);
 			const id = await this.source.addItem(collection, data);
-			console.log("ADD", key, "DATA:", data, "ID:", id);
+			console.debug("✔ ADD", collection, id, data);
 			return id;
 		} catch (reason) {
-			console.error("ADD", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ ADD", collection, data, reason);
 			throw reason;
 		}
 	}
 	async setItem(collection: string, id: string, data: Data): Promise<void> {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("SET:", key, "DATA:", data);
+			console.debug("⋯ SET", collection, id, data);
 			await this.source.setItem(collection, id, data);
-			console.log("SET:", key, "DATA:", data, "DONE:", 1);
+			console.debug("✔ SET", collection, id, data, 1);
 		} catch (reason) {
-			console.error("SET:", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ SET", collection, id, data, reason);
 			throw reason;
 		}
 	}
 	async updateItem(collection: string, id: string, updates: Updates): Promise<void> {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("UPDATE:", key, "UPDATES:", updates);
+			console.debug("⋯ UPDATE", collection, id, updates);
 			await this.source.updateItem(collection, id, updates);
-			console.log("UPDATE:", key, "UPDATES:", updates, "DONE:", 1);
+			console.debug("✔ UPDATE", collection, id, updates, 1);
 		} catch (reason) {
-			console.error("UPDATE:", key, "UPDATES:", updates, "ERROR:", reason);
+			console.error("✘ UPDATE", collection, id, updates, reason);
 			throw reason;
 		}
 	}
 	async deleteItem(collection: string, id: string): Promise<void> {
-		const key = _getItemKey(collection, id);
 		try {
-			console.log("DELETE:", key);
+			console.debug("⋯ DELETE", collection, id);
 			await this.source.deleteItem(collection, id);
-			console.log("DELETE:", key, "DONE:", 1);
+			console.debug("✔ DELETE", collection, id, 1);
 		} catch (reason) {
-			console.error("DELETE:", key, "ERROR:", reason);
+			console.error("✘ DELETE", collection, id, reason);
 			throw reason;
 		}
 	}
 	async getQuery(collection: string, query: ItemQuery): Promise<ItemArray> {
-		const key = _getQueryKey(collection, query);
 		try {
-			console.log("GET:", key);
+			console.debug("⋯ GET", collection, query);
 			const items = await this.source.getQuery(collection, query);
-			console.log("GET:", key, "ITEMS:", items);
+			console.debug("✔ GET", collection, query, "ITEMS", items);
 			return items;
 		} catch (reason) {
-			console.error("GET:", key, "ERROR:", reason);
+			console.error("✘ GET", collection, query, reason);
 			throw reason;
 		}
 	}
 	async setQuery(collection: string, query: ItemQuery, data: Data): Promise<number> {
-		const key = _getQueryKey(collection, query);
 		try {
-			console.log("SET:", key, "DATA:", data);
+			console.debug("⋯ SET", collection, query, data);
 			const num = await this.source.setQuery(collection, query, data);
-			console.log("SET:", key, "DATA:", data, "DONE:", num);
+			console.debug("✔ SET", collection, query, data, num);
 			return num;
 		} catch (reason) {
-			console.error("SET:", key, "DATA:", data, "ERROR:", reason);
+			console.error("✘ SET", collection, query, data, reason);
 			throw reason;
 		}
 	}
 	async updateQuery(collection: string, query: ItemQuery, updates: Updates): Promise<number> {
-		const key = _getQueryKey(collection, query);
 		try {
-			console.log("UPDATE:", key, "UPDATES:", updates);
+			console.debug("⋯ UPDATE", collection, query, updates);
 			const num = await this.source.updateQuery(collection, query, updates);
-			console.log("UPDATE:", key, "UPDATES:", updates, "DONE:", num);
+			console.debug("✔ UPDATE", collection, query, updates, num);
 			return num;
 		} catch (reason) {
-			console.error("UPDATE:", key, "UPDATES:", updates, "ERROR:", reason);
+			console.error("✘ UPDATE", collection, query, updates, reason);
 			throw reason;
 		}
 	}
 	async deleteQuery(collection: string, query: ItemQuery): Promise<number> {
-		const key = _getQueryKey(collection, query);
 		try {
-			console.log("DELETE:", key);
+			console.debug("⋯ DELETE", collection, query);
 			const num = await this.source.deleteQuery(collection, query);
-			console.log("DELETE:", key, "DONE:", num);
+			console.debug("✔ DELETE", collection, query, num);
 			return num;
 		} catch (reason) {
-			console.error("DELETE:", key, "ERROR:", reason);
+			console.error("✘ DELETE", collection, query, reason);
 			throw reason;
 		}
 	}
 }
-
-const _getItemKey = (collection: string, id: string): string => `${collection}/${id}`;
-const _getQueryKey = (collection: string, query: ItemQuery): string => `${collection}?${JSON.stringify(query)}`;
