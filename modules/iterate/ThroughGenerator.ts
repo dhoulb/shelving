52d4401b1b40fb736d172ffa65d8d65f524ac9b1
dhoulb <dave@shax.com>
2023-07-11 11:59:45 +0100

    refactor(iterate): Better variable name

--- modules/iterate/ThroughGenerator.ts
+++ modules/iterate/ThroughGenerator.ts
@@ -2,20 +2,20 @@ import { AbstractGenerator } from "./AbstractGenerator.js";
 
 /** Iterable that pulls values from a source iterable. */
 export class ThroughGenerator<T, R, N> extends AbstractGenerator<T, R, N> {
-	private readonly _iterator: Iterator<T, R, N>;
+	private readonly _source: Iterator<T, R, N>;
 	constructor(iterator: Iterator<T, R, N>) {
 		super();
-		this._iterator = iterator;
+		this._source = iterator;
 	}
 
 	// Implement `AbstractGenerator`
 	next(value: N): IteratorResult<T, R> {
-		return this._iterator.next(value);
+		return this._source.next(value);
 	}
 	override throw(thrown: Error | unknown): IteratorResult<T, R> {
-		return this._iterator.throw ? this._iterator.throw(thrown) : super.throw(thrown);
+		return this._source.throw ? this._source.throw(thrown) : super.throw(thrown);
 	}
 	override return(value: R): IteratorResult<T, R> {
-		return this._iterator.return ? this._iterator.return(value) : super.return(value);
+		return this._source.return ? this._source.return(value) : super.return(value);
 	}
 }
