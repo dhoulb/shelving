fd8460d9f29fab4d91bc1e7a62ae0a0224fc6235
dhoulb <dave@shax.com>
2023-08-09 15:10:53 +0100

    fix(provider): Additional overrides to allow easier typed values in `Provider`

--- modules/provider/Provider.ts
+++ modules/provider/Provider.ts
@@ -81,26 +81,44 @@ abstract class AbstractProvider {
 
 /** Provider with a fully synchronous interface */
 export abstract class Provider extends AbstractProvider {
+	abstract override getItem<T extends Data>(collection: string, id: string): ItemValue<T>;
 	abstract override getItem(collection: string, id: string): ItemValue;
+	abstract override addItem<T extends Data>(collection: string, data: T): string;
 	abstract override addItem(collection: string, data: Data): string;
+	abstract override setItem<T extends Data>(collection: string, id: string, data: T): void;
 	abstract override setItem(collection: string, id: string, data: Data): void;
+	abstract override updateItem<T extends Data>(collection: string, id: string, updates: Updates<T>): void;
 	abstract override updateItem(collection: string, id: string, updates: Updates): void;
+	abstract override deleteItem<T extends Data>(collection: string, id: string): void; // eslint-disable-line @typescript-eslint/no-unused-vars
 	abstract override deleteItem(collection: string, id: string): void;
+	abstract override getQuery<T extends Data>(collection: string, query: ItemQuery<T>): ItemArray;
 	abstract override getQuery(collection: string, query: ItemQuery): ItemArray;
+	abstract override setQuery<T extends Data>(collection: string, query: ItemQuery<T>, data: T): number;
 	abstract override setQuery(collection: string, query: ItemQuery, data: Data): number;
+	abstract override updateQuery<T extends Data>(collection: string, query: ItemQuery<T>, updates: Updates<T>): number;
 	abstract override updateQuery(collection: string, query: ItemQuery, updates: Updates): number;
+	abstract override deleteQuery<T extends Data>(collection: string, query: ItemQuery<T>): number;
 	abstract override deleteQuery(collection: string, query: ItemQuery): number;
 }
 
 /** Provider with a fully asynchronous interface */
 export abstract class AsyncProvider extends AbstractProvider {
+	abstract override getItem<T extends Data>(collection: string, id: string): Promise<ItemValue<T>>;
 	abstract override getItem(collection: string, id: string): Promise<ItemValue>;
+	abstract override addItem<T extends Data>(collection: string, data: T): Promise<string>;
 	abstract override addItem(collection: string, data: Data): Promise<string>;
+	abstract override setItem<T extends Data>(collection: string, id: string, data: T): Promise<void>;
 	abstract override setItem(collection: string, id: string, data: Data): Promise<void>;
+	abstract override updateItem<T extends Data>(collection: string, id: string, updates: Updates<T>): Promise<void>;
 	abstract override updateItem(collection: string, id: string, updates: Updates): Promise<void>;
+	abstract override deleteItem<T extends Data>(collection: string, id: string): Promise<void>; // eslint-disable-line @typescript-eslint/no-unused-vars
 	abstract override deleteItem(collection: string, id: string): Promise<void>;
+	abstract override getQuery<T extends Data>(collection: string, query: ItemQuery<T>): Promise<ItemArray>;
 	abstract override getQuery(collection: string, query: ItemQuery): Promise<ItemArray>;
+	abstract override setQuery<T extends Data>(collection: string, query: ItemQuery<T>, data: T): Promise<number>;
 	abstract override setQuery(collection: string, query: ItemQuery, data: Data): Promise<number>;
+	abstract override updateQuery<T extends Data>(collection: string, query: ItemQuery<T>, updates: Updates<T>): Promise<number>;
 	abstract override updateQuery(collection: string, query: ItemQuery, updates: Updates): Promise<number>;
+	abstract override deleteQuery<T extends Data>(collection: string, query: ItemQuery<T>): Promise<number>;
 	abstract override deleteQuery(collection: string, query: ItemQuery): Promise<number>;
 }
