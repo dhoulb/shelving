71f72297ff57b448be28d4f6e319dc93e367198d
dhoulb <dave@shax.com>
2023-12-18 14:31:44 +0000

    fix(schema): Add tests for `FileSchema`

---
+++ modules/schema/FileSchema.test.ts
@@ -0,0 +1,56 @@
+import type { Schema } from "../index.js";
+import { FILE, Feedback, FileSchema, OPTIONAL_FILE } from "../index.js";
+
+// Tests.
+test("TypeScript", () => {
+	const s1: Schema<string | null> = OPTIONAL_FILE;
+	const r1: string | null = s1.validate("test.xml");
+
+	const s2: Schema<string> = FILE;
+	const r2: string = s2.validate("test.xml");
+
+	const s3: Schema<string> = new FileSchema({});
+	const r3: string = s3.validate("test.xml");
+});
+test("constructor()", () => {
+	const schema1 = new FileSchema({});
+	expect(schema1).toBeInstanceOf(FileSchema);
+	const schema2 = FILE;
+	expect(schema2).toBeInstanceOf(FileSchema);
+	const schema3 = FILE;
+	expect(schema3).toBeInstanceOf(FileSchema);
+});
+describe("validate()", () => {
+	describe("FileSchema without types", () => {
+		const schema = new FileSchema({});
+		test("Valid files are valid", () => {
+			expect(schema.validate("file.txt")).toBe("file.txt");
+			expect(schema.validate("file.xml")).toBe("file.xml");
+		});
+		test("Invalid files are invalid", () => {
+			expect(() => schema.validate("file")).toThrow(Feedback);
+			expect(() => schema.validate("file.")).toThrow(Feedback);
+		});
+	});
+	describe("FileSchema with types", () => {
+		const schema = new FileSchema({ types: { txt: "text/plain", xml: "application/xml" } });
+		test("Valid files are valid", () => {
+			expect(schema.validate("file.txt")).toBe("file.txt");
+			expect(schema.validate("file.xml")).toBe("file.xml");
+		});
+		test("Invalid files are invalid for format reasons", () => {
+			expect(() => schema.validate("file")).toThrow(Feedback);
+			expect(() => schema.validate("file.")).toThrow(Feedback);
+		});
+		test("Invalid files are invalid for type reasons", () => {
+			expect(() => schema.validate("file.js")).toThrow(Feedback);
+			expect(() => schema.validate("file.png")).toThrow(Feedback);
+		});
+	});
+});
+describe("options.value", () => {
+	test("Undefined with default value returns default value", () => {
+		const schema = new FileSchema({ value: "file.txt" });
+		expect(schema.validate(undefined)).toBe("file.txt");
+	});
+});
--- modules/schema/FileSchema.ts
+++ modules/schema/FileSchema.ts
@@ -2,29 +2,32 @@ import type { FileTypes } from "../util/file.js";
 import { Feedback } from "../feedback/Feedback.js";
 import { getOptionalFileExtension } from "../util/file.js";
 import { isProp } from "../util/object.js";
-import { getValidationContext } from "../util/validate.js";
+import { OPTIONAL } from "./OptionalSchema.js";
 import { StringSchema, type StringSchemaOptions } from "./StringSchema.js";
 
 /** Allowed options for `FileSchema` */
 export type FileSchemaOptions = StringSchemaOptions & {
-	readonly formats: FileTypes;
+	readonly types?: FileTypes | undefined;
 };
 
 /** Validate a file name matching one or more extensions. */
 export class FileSchema extends StringSchema {
-	readonly formats: FileTypes;
-	constructor(options: { formats: FileTypes } & StringSchemaOptions) {
-		super(options);
-		this.formats = options.formats;
+	readonly types: FileTypes | undefined;
+	constructor({ types, title = "File", ...options }: FileSchemaOptions) {
+		super({ title, ...options });
+		this.types = types;
 	}
 	override validate(unsafeValue: unknown = this.value): string {
 		const path = super.validate(unsafeValue);
-		const { action } = getValidationContext();
-		if (action === "set" || action === "update") {
-			const extension = getOptionalFileExtension(path);
-			if (!extension) throw new Feedback("Must be file name with extension", unsafeValue);
-			if (!isProp(this.formats, extension)) throw new Feedback("Invalid file type", extension);
-		}
+		const extension = getOptionalFileExtension(path);
+		if (!extension) throw new Feedback("Must be file name with extension", unsafeValue);
+		if (this.types && !isProp(this.types, extension)) throw new Feedback("Invalid file type", extension);
 		return path;
 	}
 }
+
+/** Valid file, e.g. `file.txt` */
+export const FILE = new FileSchema({});
+
+/** Valid optional file, e.g. `file.txt`, or `null` */
+export const OPTIONAL_FILE = OPTIONAL(FILE);
