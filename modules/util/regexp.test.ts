6967e795ebdde448ef128c1fb962b783bd755747
dhoulb <dave@shax.com>
2024-03-01 17:01:42 +0000

    feat(util/regexp): Add `getMatch()` and `getOptionalMatch()` helper functions

--- modules/util/regexp.test.ts
+++ modules/util/regexp.test.ts
@@ -1,4 +1,4 @@
-import { filterArray, getAllRegExp, getAnyRegExp, isRegExpMatch, notRegExpMatch } from "../index.js";
+import { filterArray, getAllRegExp, getAnyRegExp, isMatch, notMatch } from "../index.js";
 
 test("getAllRegExp()", () => {
 	// No pattern (always true).
@@ -30,27 +30,27 @@ test("getAnyRegExp()", () => {
 	expect(getAnyRegExp(["tapir", "cat"]).test("the dog and the cat")).toBe(true);
 	expect(getAnyRegExp(["tapir", "elephant"]).test("the dog and the cat")).toBe(false);
 });
-test("isRegExpMatch()", () => {
+test("isMatch()", () => {
 	const arr = ["the dog", "the man", "the cat"];
 
-	expect(filterArray(arr, isRegExpMatch, /dog/)).toEqual(["the dog"]);
-	expect(filterArray(arr, isRegExpMatch, /man/)).toEqual(["the man"]);
-	expect(filterArray(arr, isRegExpMatch, /tapir/)).toEqual([]);
-	expect(filterArray(arr, isRegExpMatch, /man|cat/)).toEqual(["the man", "the cat"]);
+	expect(filterArray(arr, isMatch, /dog/)).toEqual(["the dog"]);
+	expect(filterArray(arr, isMatch, /man/)).toEqual(["the man"]);
+	expect(filterArray(arr, isMatch, /tapir/)).toEqual([]);
+	expect(filterArray(arr, isMatch, /man|cat/)).toEqual(["the man", "the cat"]);
 
 	// Returs same instance if no filtering needed.
-	expect(filterArray(arr, isRegExpMatch, /the/)).toBe(arr);
-	expect(filterArray(arr, isRegExpMatch, /dog|man|cat/)).toBe(arr);
+	expect(filterArray(arr, isMatch, /the/)).toBe(arr);
+	expect(filterArray(arr, isMatch, /dog|man|cat/)).toBe(arr);
 });
-test("notRegExpMatch()", () => {
+test("notMatch()", () => {
 	const arr = ["the dog", "the man", "the cat"];
 
-	expect(filterArray(arr, notRegExpMatch, /dog/)).toEqual(["the man", "the cat"]);
-	expect(filterArray(arr, notRegExpMatch, /man/)).toEqual(["the dog", "the cat"]);
-	expect(filterArray(arr, notRegExpMatch, /man|cat/)).toEqual(["the dog"]);
-	expect(filterArray(arr, notRegExpMatch, /the/)).toEqual([]);
-	expect(filterArray(arr, notRegExpMatch, /dog|man|cat/)).toEqual([]);
+	expect(filterArray(arr, notMatch, /dog/)).toEqual(["the man", "the cat"]);
+	expect(filterArray(arr, notMatch, /man/)).toEqual(["the dog", "the cat"]);
+	expect(filterArray(arr, notMatch, /man|cat/)).toEqual(["the dog"]);
+	expect(filterArray(arr, notMatch, /the/)).toEqual([]);
+	expect(filterArray(arr, notMatch, /dog|man|cat/)).toEqual([]);
 
 	// Returs same instance if no filtering needed.
-	expect(filterArray(arr, notRegExpMatch, /tapir/)).toBe(arr);
+	expect(filterArray(arr, notMatch, /tapir/)).toBe(arr);
 });
--- modules/util/regexp.ts
+++ modules/util/regexp.ts
@@ -57,22 +57,12 @@ export function getAllRegExp(patterns: Iterable<PossibleRegExp> & NotString, fla
 	return new RegExp(`^(?=.*?(?:${getArray(patterns).map(getRegExpSource).join("))(?=.*?(?:")}))`, flags);
 }
 
-/** Match function for finding strings that match against regular expressions (use with `filter()` to positively filter iterable sets of items). */
-export function isRegExpMatch(item: string, target: RegExp): boolean {
-	return target.test(item);
-}
-
-/** Match function for finding strings that match against regular expressions (use with `filter()` to negatively filter iterable sets of items). */
-export function notRegExpMatch(item: string, target: RegExp): boolean {
-	return !target.test(item);
-}
-
-/** Regular expression match array that you've asserted contains the specified named groups. */
+/** Regular expression match array that matches a specific string format. */
 export interface TypedRegExpExecArray<T extends string = string> extends RegExpExecArray {
 	0: T;
 }
 
-/** Regular expression that you've asserted contains the specified named capture groups. */
+/** Regular expression that matches a specific string format. */
 export interface TypedRegExp<T extends string = string> extends RegExp {
 	exec(input: string): TypedRegExpExecArray<T> | null;
 }
@@ -80,12 +70,56 @@ export interface TypedRegExp<T extends string = string> extends RegExp {
 /** Set of named match groups from a regular expression. */
 export type NamedRegExpData = { [named: string]: string };
 
-/** Regular expression match array that you've asserted contains the specified named groups. */
+/** Regular expression match array that contains the specified named groups. */
 export interface NamedRegExpExecArray<T extends NamedRegExpData = NamedRegExpData> extends RegExpExecArray {
 	groups: T; // Groups is always set if a single `(?<named> placeholder)` appears in the RegExp.
 }
 
-/** Regular expression that you've asserted contains the specified named capture groups. */
+/** Regular expression that contains the specified named capture groups. */
 export interface NamedRegExp<T extends NamedRegExpData = NamedRegExpData> extends RegExp {
 	exec(input: string): NamedRegExpExecArray<T> | null;
+}
+
+/** Match function for finding strings that match against regular expressions (use with `filter()` to positively filter iterable sets of items). */
+export function isMatch(str: string, regexp: RegExp): boolean {
+	return regexp.test(str);
+}
+
+/** Match function for finding strings that match against regular expressions (use with `filter()` to negatively filter iterable sets of items). */
+export function notMatch(str: string, regexp: RegExp): boolean {
+	return !regexp.test(str);
+}
+
+/** Get an optional regular expression match, or `undefined` if no match could be made. */
+export function getOptionalMatch<T extends NamedRegExpData>(str: string, regexp: NamedRegExp<T>): NamedRegExpExecArray<T> | undefined;
+export function getOptionalMatch<T extends string>(str: string, regexp: TypedRegExp<T>): TypedRegExpExecArray<T> | undefined;
+export function getOptionalMatch(str: string, regexp: RegExp): RegExpExecArray | undefined;
+export function getOptionalMatch(str: string, regexp: RegExp): RegExpExecArray | undefined {
+	return regexp.exec(str) || undefined;
+}
+
+/** Get a required regular expression match, or throw `ValueError` if no match could be made. */
+export function getMatch<T extends NamedRegExpData>(str: string, regexp: NamedRegExp<T>): NamedRegExpExecArray<T>;
+export function getMatch<T extends string>(str: string, regexp: TypedRegExp<T>): TypedRegExpExecArray<T>;
+export function getMatch(str: string, regexp: RegExp): RegExpExecArray;
+export function getMatch(str: string, regexp: RegExp): RegExpExecArray {
+	const match = getOptionalMatch(str, regexp);
+	if (!match) throw new ValueError("Must match regular expression", str);
+	return match;
+}
+
+/** Get an optional regular expression match, or `undefined` if no match could be made. */
+export function getOptionalMatchGroups<T extends NamedRegExpData>(str: string, regexp: NamedRegExp<T>): T | undefined;
+export function getOptionalMatchGroups(str: string, regexp: RegExp): NamedRegExpData | undefined;
+export function getOptionalMatchGroups(str: string, regexp: RegExp): NamedRegExpData | undefined {
+	return regexp.exec(str)?.groups || undefined;
+}
+
+/** Get a required regular expression match, or throw `ValueError` if no match could be made. */
+export function getMatchGroups<T extends NamedRegExpData>(str: string, regexp: NamedRegExp<T>): T;
+export function getMatchGroups(str: string, regexp: RegExp): NamedRegExpData;
+export function getMatchGroups(str: string, regexp: RegExp): NamedRegExpData {
+	const groups = getOptionalMatchGroups(str, regexp);
+	if (!groups) throw new ValueError("Must match regular expression", str);
+	return groups;
 }
