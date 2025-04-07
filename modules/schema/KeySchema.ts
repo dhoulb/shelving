dfe7df7ca0e699a03ade270d19f5559ffae667f8
Dave Houlbrooke <dave@shax.com>
2025-04-07 18:39:23 +0100

    feat: Make `KeySchema` strip invalid characters (only [a-zA-Z0-9] allowed

--- modules/schema/KeySchema.ts
+++ modules/schema/KeySchema.ts
@@ -1,20 +1,27 @@
 import { OPTIONAL } from "./OptionalSchema.js";
 import { StringSchema, type StringSchemaOptions } from "./StringSchema.js";
 
+const R_NOT_CHAR = /[^a-zA-Z0-9]/g;
+
 /**
  * Define a valid database key.
  *
+ * - Characters that are not a-z, A-Z, 0-9 are removed.
  * - Default minimum key length is 1 character.
- * - Default maximum key length is 64 characters.
+ * - Default maximum key length is 32 characters.
+ * - 32 characters is enough for UUIDs, as the 4 `-` hyphens are removed.
  */
 export class KeySchema extends StringSchema {
-	constructor({ min = 1, max = 64, ...options }: StringSchemaOptions) {
+	constructor({ min = 1, max = 32, ...options }: StringSchemaOptions) {
 		super({ min, max, ...options });
+	}
+	override sanitize(str: string): string {
+		return str.replace(R_NOT_CHAR, "");
 	}
 }
 
 /** Valid database key. */
-export const KEY = new KeySchema({});
+export const KEY = new KeySchema({ title: "ID" });
 
 /** Valid optional database key. */
 export const OPTIONAL_KEY = OPTIONAL(KEY);
