5854b419633193be738cf873645fe5600776f0ee
Dave Houlbrooke <dave@shax.com>
2025-04-09 18:50:00 +0100

    fix: Correct code for `UnauthorizedError`

--- modules/error/UnauthorizedError.ts
+++ modules/error/UnauthorizedError.ts
@@ -2,7 +2,7 @@ import { EnhancedError } from "./EnhancedError.js";
 
 /** Thrown if an operation failed because the user is logged in but does not have sufficient privileges to access something. */
 export class UnauthorizedError extends EnhancedError {
-	override readonly code = 403;
+	override readonly code = 401;
 	constructor(message = "Unauthorized", context?: unknown) {
 		super(message, context);
 	}
