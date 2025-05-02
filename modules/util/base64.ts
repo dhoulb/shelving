df87d773e5d6da8ebcdf8c3baacfaffe0676cd96
Dave Houlbrooke <dave@shax.com>
2025-05-02 15:32:11 +0100

    fix: Tidy up base64 utils

--- modules/util/base64.ts
+++ modules/util/base64.ts
@@ -1,19 +1,45 @@
+import { ValueError } from "../error/ValueError.js";
+import type { AnyFunction } from "./function.js";
+
 /** Encode a string to Base64 (with no `=` padding on the end). */
 export function encodeBase64(str: string): string {
-	return btoa(str).replace(/=+$/, "");
+	return _encodeBase64(encodeBase64, str);
+}
+function _encodeBase64(caller: AnyFunction, str: string): string {
+	try {
+		return btoa(str).replace(/=+$/, "");
+	} catch (thrown) {
+		console.error(thrown);
+		throw new ValueError("String contains invalid characters", {
+			received: str,
+			cause: thrown,
+			caller,
+		});
+	}
 }
 
 /** Decode a string from Base64 (strips `=` padding on the end). */
-export function decodeBase64(b64: string): string {
-	return atob(b64.replace(/=+$/, ""));
+export function decodeBase64(base64: string): string {
+	return _decodeBase64(decodeBase64, base64);
+}
+function _decodeBase64(caller: AnyFunction, base64: string): string {
+	try {
+		return atob(base64.replace(/=+$/, ""));
+	} catch (thrown) {
+		throw new ValueError("Base64 string is not correctly encoded", {
+			received: base64,
+			cause: thrown,
+			caller,
+		});
+	}
 }
 
 /** Encode a string to URL-safe Base64 */
 export function encodeBase64Url(str: string): string {
-	return encodeBase64(str).replace(/\+/g, "-").replace(/\//g, "_");
+	return _encodeBase64(encodeBase64Url, str).replace(/\+/g, "-").replace(/\//g, "_");
 }
 
 /** Decode a string from URL-safe Base64. */
 export function decodeBase64Url(b64: string): string {
-	return decodeBase64(b64.replace(/-/g, "+").replace(/_/g, "/"));
+	return _decodeBase64(decodeBase64Url, b64.replace(/-/g, "+").replace(/_/g, "/"));
 }
