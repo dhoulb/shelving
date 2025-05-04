bfa044f8c7b22c2f26821b96b61da2c92a428496
Dave Houlbrooke <dave@shax.com>
2025-05-04 14:31:46 +0100

    refactor(util/jwt): Improve constant names for consistency with crypto utils

--- modules/util/jwt.ts
+++ modules/util/jwt.ts
@@ -7,12 +7,14 @@ import type { Data } from "./data.js";
 import type { AnyFunction } from "./function.js";
 
 // Constants.
-const TOKEN_HEADER = { alg: "HS512", typ: "JWT" };
-const TOKEN_EXPIRY_MS = DAY * 10;
-const TOKEN_MINIMUM_SECRET = 16;
+const HASH = "SHA-512";
+const ALGORITHM = { name: "HMAC", hash: HASH };
+const HEADER = { alg: "HS512", typ: "JWT" };
+const EXPIRY_MS = DAY * 10;
+const MIN_SECRET = 16;
 
 function _getKey(secret: string, ...usages: KeyUsage[]): Promise<CryptoKey> {
-	return crypto.subtle.importKey("raw", requireBytes(secret), { name: "HMAC", hash: { name: "SHA-512" } }, false, usages);
+	return crypto.subtle.importKey("raw", requireBytes(secret), ALGORITHM, false, usages);
 }
 
 /**
@@ -22,18 +24,18 @@ function _getKey(secret: string, ...usages: KeyUsage[]): Promise<CryptoKey> {
  * @throws ValueError If the input parameters, e.g. `secret` or `issuer`, are invalid.
  */
 export async function encodeToken(claims: Data, secret: string): Promise<string> {
-	if (typeof secret !== "string" || secret.length < TOKEN_MINIMUM_SECRET)
-		throw new ValueError(`JWT secret must be string with minimum ${TOKEN_MINIMUM_SECRET} characters`, {
+	if (typeof secret !== "string" || secret.length < MIN_SECRET)
+		throw new ValueError(`JWT secret must be string with minimum ${MIN_SECRET} characters`, {
 			received: secret,
 			caller: encodeToken,
 		});
 
 	// Encode header.
-	const header = encodeBase64Url(JSON.stringify(TOKEN_HEADER));
+	const header = encodeBase64Url(JSON.stringify(HEADER));
 
 	// Encode payload.
 	const iat = Math.floor(Date.now() / 1000);
-	const exp = Math.floor(iat + TOKEN_EXPIRY_MS / 1000);
+	const exp = Math.floor(iat + EXPIRY_MS / 1000);
 	const payload = encodeBase64Url(JSON.stringify({ iat, exp, ...claims }));
 
 	// Create signature.
@@ -95,8 +97,8 @@ export function _splitToken(caller: AnyFunction, token: unknown): TokenData {
  * @throws UnauthorizedError If the token signature is incorrect, token is expired or not issued yet.
  */
 export async function verifyToken(token: unknown, secret: string): Promise<Data> {
-	if (typeof secret !== "string" || secret.length < TOKEN_MINIMUM_SECRET)
-		throw new ValueError(`JWT secret must be string with minimum ${TOKEN_MINIMUM_SECRET} characters`, {
+	if (typeof secret !== "string" || secret.length < MIN_SECRET)
+		throw new ValueError(`JWT secret must be string with minimum ${MIN_SECRET} characters`, {
 			received: secret,
 			caller: verifyToken,
 		});
@@ -104,10 +106,10 @@ export async function verifyToken(token: unknown, secret: string): Promise<Data>
 	const { header, payload, signature, headerData, payloadData } = _splitToken(verifyToken, token);
 
 	// Validate header.
-	if (headerData.typ !== TOKEN_HEADER.typ)
-		throw new RequestError(`JWT header type must be \"${TOKEN_HEADER.typ}\"`, { received: headerData.typ, caller: verifyToken });
-	if (headerData.alg !== TOKEN_HEADER.alg)
-		throw new RequestError(`JWT header algorithm must be \"${TOKEN_HEADER.alg}\"`, { received: headerData.alg, caller: verifyToken });
+	if (headerData.typ !== HEADER.typ)
+		throw new RequestError(`JWT header type must be \"${HEADER.typ}\"`, { received: headerData.typ, caller: verifyToken });
+	if (headerData.alg !== HEADER.alg)
+		throw new RequestError(`JWT header algorithm must be \"${HEADER.alg}\"`, { received: headerData.alg, caller: verifyToken });
 
 	// Validate signature.
 	const key = await _getKey(secret, "verify");
