30c1aed0751f3e886be8d25cc371e9f61281d925
Dave Houlbrooke <dave@shax.com>
2025-05-06 00:11:47 +0100

    fix(util/crypto): Export crypto utils in index

--- modules/util/index.ts
+++ modules/util/index.ts
@@ -8,6 +8,7 @@ export * from "./callback.js";
 export * from "./class.js";
 export * from "./color.js";
 export * from "./constants.js";
+export * from "./crypto.js";
 export * from "./data.js";
 export * from "./date.js";
 export * from "./debug.js";
