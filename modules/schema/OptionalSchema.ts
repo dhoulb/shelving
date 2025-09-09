da0caae58f669c699e885cab843fee055c9289f2
Dave Houlbrooke <dave@shax.com>
2025-09-09 16:27:27 +0100

    refactor: Spacing

--- modules/schema/OptionalSchema.ts
+++ modules/schema/OptionalSchema.ts
@@ -6,13 +6,12 @@ import { ThroughSchema } from "./ThroughSchema.js";
  * - If the prop is `undefined`, then `undefined` is returned.
  * - When used with `validateData()` this means the prop can be silently skipped.
  */
-
 export class OptionalSchema<T> extends ThroughSchema<T | undefined> {
 	override validate(unsafeValue: unknown): T | undefined {
 		if (unsafeValue === undefined) return undefined;
 		return super.validate(unsafeValue);
 	}
 }
-/** Make a property of a set of data optional, i.e. it can be the value or `undefined` */
 
+/** Make a property of a set of data optional, i.e. it can be the value or `undefined` */
 export const OPTIONAL = <T>(source: Schema<T>): OptionalSchema<T> => new OptionalSchema({ source });
