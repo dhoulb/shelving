1042eb819ecd939b87d44ece2af1b6d36b9838ce
Dave Houlbrooke <dave@shax.com>
2025-09-26 17:49:49 +0100

    feat: `TimeSchema` extends `DateSchema` (should make significantly simpler to implement fields

--- modules/schema/TimeSchema.ts
+++ modules/schema/TimeSchema.ts
@@ -1,43 +1,34 @@
 import { ValueFeedback } from "../feedback/Feedback.js";
 import { type PossibleDate, getDate, requireTime } from "../util/date.js";
 import { formatTime } from "../util/format.js";
-import type { Nullish } from "../util/null.js";
 import { roundStep } from "../util/number.js";
+import { DateSchema, type DateSchemaOptions } from "./DateSchema.js";
 import { NULLABLE } from "./NullableSchema.js";
-import type { SchemaOptions } from "./Schema.js";
-import { Schema } from "./Schema.js";
 
 /** Allowed options for `TimeSchama` */
-export interface TimeSchemaOptions extends SchemaOptions {
-	readonly value?: PossibleDate | undefined;
-	readonly min?: Nullish<PossibleDate>;
-	readonly max?: Nullish<PossibleDate>;
-	readonly step?: number | undefined;
+export interface TimeSchemaOptions extends DateSchemaOptions {
+	step?: number | undefined;
 }
 
 /** Define a valid time in 24h hh:mm:ss.fff format, e.g. `23:59` or `24:00 */
-export class TimeSchema extends Schema<string> {
+export class TimeSchema extends DateSchema {
 	declare readonly value: PossibleDate;
-	readonly min: Date | undefined;
-	readonly max: Date | undefined;
 	/**
 	 * Rounding step (in milliseconds, because that's the base unit for time), e.g. `60000` will round to the nearest second.
 	 * - Note: `<input type="time">` elements expect `step=""` to be  in _seconds_ so you need to multiply this by `1000`
 	 */
 	readonly step: number | undefined;
-	constructor({ min, max, step = 1000, title = "Time", value = "now", ...options }: TimeSchemaOptions) {
-		super({ title, value, ...options });
-		this.min = getDate(min);
-		this.max = getDate(max);
+	constructor({ title = "Time", step, ...options }: TimeSchemaOptions) {
+		super({ title, ...options });
 		this.step = step;
 	}
 	override validate(unsafeValue: unknown = this.value): string {
 		const date = getDate(unsafeValue);
 		if (!date) throw new ValueFeedback(unsafeValue ? "Invalid time" : "Required", unsafeValue);
-		const roundedTime = typeof this.step === "number" ? new Date(roundStep(date.getTime(), this.step)) : date;
-		if (this.max && roundedTime > this.max) throw new ValueFeedback(`Maximum ${formatTime(this.max)}`, roundedTime);
-		if (this.min && roundedTime < this.min) throw new ValueFeedback(`Minimum ${formatTime(this.min)}`, roundedTime);
-		return requireTime(roundedTime);
+		const rounded = typeof this.step === "number" ? new Date(roundStep(date.getTime(), this.step)) : date;
+		if (this.max && rounded > this.max) throw new ValueFeedback(`Maximum ${formatTime(this.max)}`, rounded);
+		if (this.min && rounded < this.min) throw new ValueFeedback(`Minimum ${formatTime(this.min)}`, rounded);
+		return requireTime(rounded);
 	}
 }
 
