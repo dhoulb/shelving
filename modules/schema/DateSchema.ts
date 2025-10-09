fb4ae81395a32c1af944a635a145d1db3cdc338d
JordanKayNC <jordan.kay@naturecan.com>
2025-10-09 13:23:57 +0100

    feat: step moved to dateSchema

--- modules/schema/DateSchema.ts
+++ modules/schema/DateSchema.ts
@@ -2,6 +2,7 @@ import { ValueFeedback } from "../feedback/Feedback.js";
 import type { PossibleDate } from "../util/date.js";
 import { getDate, requireDateString } from "../util/date.js";
 import { formatDate } from "../util/format.js";
+import { roundStep } from "../util/number.js";
 import type { Nullish } from "../util/null.js";
 import { NULLABLE } from "./NullableSchema.js";
 import type { SchemaOptions } from "./Schema.js";
@@ -12,36 +13,52 @@ export type DateInputType = "time" | "date" | "datetime-local";
 
 /** Allowed options for `DateSchema` */
 export interface DateSchemaOptions extends SchemaOptions {
-	readonly value?: PossibleDate | undefined;
-	readonly min?: Nullish<PossibleDate>;
-	readonly max?: Nullish<PossibleDate>;
-	readonly input?: DateInputType | undefined;
+    readonly value?: PossibleDate | undefined;
+    readonly min?: Nullish<PossibleDate>;
+    readonly max?: Nullish<PossibleDate>;
+    readonly input?: DateInputType | undefined;
+    /**
+     * Rounding step (in milliseconds, because that's the base unit for time).
+     * - E.g. `1000 * 60` will round to the nearest minute.
+     * - Note: HTML `<input>` `step` attributes are in _seconds_, so you may need to convert units.
+     */
+    readonly step?: number | undefined;
 }
 
 export class DateSchema extends Schema<string> {
-	declare readonly value: PossibleDate;
-	readonly min: Date | undefined;
-	readonly max: Date | undefined;
-	readonly input: DateInputType;
-	constructor({ min, max, value = "now", input = "date", ...options }: DateSchemaOptions) {
-		super({ title: "Date", value, ...options });
-		this.min = getDate(min);
-		this.max = getDate(max);
-		this.input = input;
-	}
-	override validate(value: unknown = this.value): string {
-		const date = getDate(value);
-		if (!date) throw new ValueFeedback(value ? "Invalid date" : "Required", value);
-		if (this.min && date < this.min) throw new ValueFeedback(`Minimum ${this.format(this.min)}`, date);
-		if (this.max && date > this.max) throw new ValueFeedback(`Maximum ${this.format(this.max)}`, date);
-		return this.stringify(date);
-	}
-	stringify(value: Date): string {
-		return requireDateString(value);
-	}
-	format(value: Date): string {
-		return formatDate(value);
-	}
+    declare readonly value: PossibleDate;
+    readonly min: Date | undefined;
+    readonly max: Date | undefined;
+    readonly input: DateInputType;
+    readonly step: number | undefined;
+
+    constructor({ min, max, value = "now", input = "date", step, ...options }: DateSchemaOptions) {
+        super({ title: "Date", value, ...options });
+        this.min = getDate(min);
+        this.max = getDate(max);
+        this.input = input;
+        this.step = step;
+    }
+
+    override validate(value: unknown = this.value): string {
+        const date = getDate(value);
+        if (!date) throw new ValueFeedback(value ? "Invalid date" : "Required", value);
+
+        const rounded = typeof this.step === "number" ? new Date(roundStep(date.getTime(), this.step)) : date;
+
+        if (this.min && rounded < this.min) throw new ValueFeedback(`Minimum ${this.format(this.min)}`, rounded);
+        if (this.max && rounded > this.max) throw new ValueFeedback(`Maximum ${this.format(this.max)}`, rounded);
+
+        return this.stringify(rounded);
+    }
+
+    stringify(value: Date): string {
+        return requireDateString(value);
+    }
+
+    format(value: Date): string {
+        return formatDate(value);
+    }
 }
 
 /** Valid date, e.g. `2005-09-12` (required because falsy values are invalid). */
--- modules/schema/TimeSchema.ts
+++ modules/schema/TimeSchema.ts
@@ -1,35 +1,21 @@
-import { ValueFeedback } from "../feedback/Feedback.js";
-import { type PossibleDate, getDate, requireTimeString } from "../util/date.js";
+import { requireTimeString } from "../util/date.js";
 import { formatTime } from "../util/format.js";
-import { roundStep } from "../util/number.js";
 import { DateSchema, type DateSchemaOptions } from "./DateSchema.js";
 import { NULLABLE } from "./NullableSchema.js";
 
-/** Allowed options for `TimeSchama` */
-export interface TimeSchemaOptions extends DateSchemaOptions {
-	step?: number | undefined;
-}
-
 /** Define a valid time in 24h hh:mm:ss.fff format, e.g. `23:59` or `24:00 */
 export class TimeSchema extends DateSchema {
-	declare readonly value: PossibleDate;
-	/**
-	 * Rounding step (in milliseconds, because that's the base unit for time), e.g. `60000` will round to the nearest second.
-	 * - Note: `<input type="time">` elements expect `step=""` to be  in _seconds_ so you need to multiply this by `1000`
-	 */
-	readonly step: number | undefined;
-	constructor({ title = "Time", input = "time", step, ...options }: TimeSchemaOptions) {
-		super({ title, input, ...options });
-		this.step = step;
-	}
-	override validate(unsafeValue: unknown = this.value): string {
-		const date = getDate(unsafeValue);
-		if (!date) throw new ValueFeedback(unsafeValue ? "Invalid time" : "Required", unsafeValue);
-		const rounded = typeof this.step === "number" ? new Date(roundStep(date.getTime(), this.step)) : date;
-		if (this.max && rounded > this.max) throw new ValueFeedback(`Maximum ${formatTime(this.max)}`, rounded);
-		if (this.min && rounded < this.min) throw new ValueFeedback(`Minimum ${formatTime(this.min)}`, rounded);
-		return requireTimeString(rounded);
-	}
+    constructor({ title = "Time", input = "time", ...options }: DateSchemaOptions) {
+        super({ title, input, ...options });
+    }
+
+    override stringify(value: Date): string {
+        return requireTimeString(value);
+    }
+
+    override format(value: Date): string {
+        return formatTime(value);
+    }
 }
 
 /** Valid time, e.g. `2005-09-12` (required because falsy values are invalid). */
