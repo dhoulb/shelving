b29229cfce7ff43637d7d5516a82cd031f84a781
Dave Houlbrooke <dave@shax.com>
2025-10-10 16:37:01 +0100

    feat: Let choice schemas take simple string arrays in addition to key/value objects

--- modules/schema/ChoiceSchema.test.ts
+++ modules/schema/ChoiceSchema.test.ts
@@ -17,9 +17,22 @@ test("Value is allowed if it exists in object options", () => {
 	const schema2 = new ChoiceSchema({ options: { 1: "A", 2: "B", 3: "C" } });
 	expect(schema2.validate("2")).toBe("2");
 });
+test("Value is allowed if it exists in array options", () => {
+	const schema1 = new ChoiceSchema({ options: ["a", "b", "c"] });
+	expect(schema1.validate("a")).toBe("a");
+	const schema2 = new ChoiceSchema({ options: ["1", "2", "3"] });
+	expect(schema2.validate("2")).toBe("2");
+});
 test("Invalid if value doesn't exist in object options", () => {
 	const schema1 = new ChoiceSchema({ options: { a: "A", b: "B", c: "C" } });
 	expect(() => schema1.validate("d")).toThrow(Feedback);
 	const schema2 = new ChoiceSchema({ options: { 1: "A", 2: "B", 3: "C" } });
+	expect(() => schema2.validate(2)).toThrow(Feedback); // Must be string.
+});
+
+test("Invalid if value doesn't exist in array options", () => {
+	const schema1 = new ChoiceSchema({ options: ["a", "b", "c"] });
+	expect(() => schema1.validate("d")).toThrow(Feedback);
+	const schema2 = new ChoiceSchema({ options: ["1", "2", "3"] });
 	expect(() => schema2.validate(2)).toThrow(Feedback); // Must be string.
 });
--- modules/schema/ChoiceSchema.ts
+++ modules/schema/ChoiceSchema.ts
@@ -1,11 +1,30 @@
 import { ValueFeedback } from "../feedback/Feedback.js";
-import { requireFirst } from "../util/array.js";
-import { getKeys, isProp } from "../util/object.js";
+import { type ImmutableArray, isArray, requireFirst } from "../util/array.js";
+import { getKeys, getProps, isProp } from "../util/object.js";
 import type { SchemaOptions } from "./Schema.js";
 import { Schema } from "./Schema.js";
 
 /* Set of options for a `ChoiceSchema` */
-export type ChoiceOptions<K extends string> = { readonly [KK in K]: unknown };
+export type ChoiceOptions<K extends string> = ImmutableArray<K> | { readonly [KK in K]: unknown };
+export type ChoiceOption<K extends string> = readonly [K, unknown];
+
+/** Get the options for a choice field as an array. */
+export function getChoiceEntries<K extends string>(options: ChoiceOptions<K>): ImmutableArray<ChoiceOption<K>> {
+	return isArray(options) ? options.map(_makeKeyEntry) : getProps(options);
+}
+function _makeKeyEntry<K extends string>(k: K): ChoiceOption<K> {
+	return [k, k];
+}
+
+/** Get the keys for a choice field as an array. */
+export function getChoiceKeys<K extends string>(options: ChoiceOptions<K>): ImmutableArray<K> {
+	return isArray(options) ? options : getKeys(options);
+}
+
+/** Get the options for a choice field as an array. */
+export function isOption<K extends string>(options: ChoiceOptions<K>, option: string): option is K {
+	return isArray(options) ? options.includes(option as K) : isProp(options, option);
+}
 
 /** Allowed options for `ChoiceSchema` */
 export interface ChoiceSchemaOptions<K extends string> extends Omit<SchemaOptions, "value"> {
@@ -16,16 +35,30 @@ export interface ChoiceSchemaOptions<K extends string> extends Omit<SchemaOption
 }
 
 /** Choose from an allowed set of values. */
-export class ChoiceSchema<K extends string> extends Schema<K> {
+export class ChoiceSchema<K extends string> extends Schema<K> implements Iterable<ChoiceOption<K>> {
 	declare readonly value: K;
 	readonly options: ChoiceOptions<K>;
-	constructor({ options, value = requireFirst(getKeys(options)), ...rest }: ChoiceSchemaOptions<K>) {
+	constructor({ options, value = requireFirst(isArray(options) ? options : getKeys(options)), ...rest }: ChoiceSchemaOptions<K>) {
 		super({ value, ...rest });
 		this.options = options;
 	}
 	validate(unsafeValue: unknown = this.value): K {
-		if (typeof unsafeValue === "string" && isProp(this.options, unsafeValue)) return unsafeValue;
+		if (typeof unsafeValue === "string" && isOption(this.options, unsafeValue)) return unsafeValue;
 		throw new ValueFeedback("Unknown value", unsafeValue);
+	}
+
+	// Implement iterable.
+	*[Symbol.iterator](): Iterator<ChoiceOption<K>> {
+		yield* getChoiceEntries(this.options);
+	}
+
+	// Get the current list of keys for this choice.
+	keys(): ImmutableArray<K> {
+		return getChoiceKeys(this.options);
+	}
+	// Get the current list of entries for this choice.
+	entries(): ImmutableArray<ChoiceOption<K>> {
+		return getChoiceEntries(this.options);
 	}
 }
 
