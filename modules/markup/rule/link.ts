6909511bd22269ca1e0ceff9935a5a1ee149eaec
Dave Houlbrooke <dave@shax.com>
2025-11-27 23:23:10 +0200

    fix: Update Markdown link elements to work with  new URI / URL code

--- modules/markup/rule/link.ts
+++ modules/markup/rule/link.ts
@@ -1,7 +1,7 @@
-import { formatURL } from "../../util/format.js";
+import { formatURI } from "../../util/format.js";
 import type { JSXElement } from "../../util/jsx.js";
-import { getLink } from "../../util/link.js";
 import { getRegExp, type NamedRegExpExecArray } from "../../util/regexp.js";
+import { getURI, HTTP_SCHEMES } from "../../util/uri.js";
 import { getURL } from "../../util/url.js";
 import { renderMarkup } from "../render.js";
 import { REACT_ELEMENT_TYPE } from "../util/internal.js";
@@ -16,10 +16,10 @@ function renderLinkMarkupRule(
 	options: MarkupOptions,
 	key: string,
 ): JSXElement {
-	const { base, schemes, hosts, rel } = options;
-	const url = getURL(unsafeHref, base);
-	const href = getLink(url, base, schemes, hosts);
-	const children = title ? renderMarkup(title, options, "link") : url ? formatURL(url) : "";
+	const { base, schemes = HTTP_SCHEMES, rel } = options;
+	const uri = getURL(unsafeHref, base) ?? getURI(unsafeHref);
+	const href = uri && schemes.includes(uri.protocol) ? uri.href : undefined;
+	const children = title ? renderMarkup(title, options, "link") : uri ? formatURI(uri) : "";
 	return {
 		key,
 		$$typeof: REACT_ELEMENT_TYPE,
--- modules/markup/util/options.ts
+++ modules/markup/util/options.ts
@@ -1,4 +1,5 @@
-import type { AbsoluteLink, LinkHosts, LinkSchemes } from "../../util/link.js";
+import type { URISchemes } from "../../util/uri.js";
+import type { URLString } from "../../util/url.js";
 import type { MarkupRules } from "./rule.js";
 
 /** The current parsing options (represents the current state of the parsing). */
@@ -13,21 +14,15 @@ export type MarkupOptions = {
 	readonly rel?: string | undefined;
 
 	/**
-	 * Set the base URL that any relative links will be relative to
+	 * Set the base URL that any relative URLs will be relative to
 	 * @default `window.location.href` in browser environments, and `undefined` in server environments.
 	 */
-	readonly base?: AbsoluteLink | undefined;
+	readonly base?: URLString | undefined;
 
 	/**
-	 * Valid URL schemes/protocols for links.
+	 * Valid URI schemes/protocols for URLs and URIs.
 	 * @example ["http:", "https:"]
 	 * @default ["http:", "https:"]
 	 */
-	readonly schemes?: LinkSchemes | undefined;
-
-	/**
-	 * Valid URL hosts for links.
-	 * @example ["google.com", "www.google.com"]
-	 */
-	readonly hosts?: LinkHosts | undefined;
+	readonly schemes?: URISchemes | undefined;
 };
