556eb82a1fa153e881b76b7662ac7a2612cc9c1f
Dave Houlbrooke <dave@shax.com>
2026-04-01 02:21:16 +0100

    fix: Update `/api` barrel

--- modules/api/index.ts
+++ modules/api/index.ts
@@ -1,8 +1,11 @@
 export * from "./cache/APICache.js";
 export * from "./cache/EndpointCache.js";
 export * from "./cache/EndpointStore.js";
+
 export * from "./endpoint/Endpoint.js";
 export * from "./endpoint/util.js";
+
+export * from "./provider/APIProvider.js";
 export * from "./provider/ClientAPIProvider.js";
 export * from "./provider/MockAPIProvider.js";
 export * from "./provider/ThroughAPIProvider.js";
