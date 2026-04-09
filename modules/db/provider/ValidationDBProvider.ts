e52f9126dbd1e099a8e0180bbae0f90227da4fa9
Dave Houlbrooke <dave@shax.com>
2026-04-09 16:18:29 +0100

    fix(db): Validation provider refactor to use `Collection` objects correctly

--- modules/db/provider/ValidationDBProvider.ts
+++ modules/db/provider/ValidationDBProvider.ts
@@ -1,38 +1,35 @@
 import { ValueError } from "../../error/ValueError.js";
-import { type DataSchema, PARTIAL } from "../../schema/DataSchema.js";
-import type { Schema } from "../../schema/Schema.js";
+import { PARTIAL } from "../../schema/DataSchema.js";
 import type { MutableArray } from "../../util/array.js";
 import type { Data } from "../../util/data.js";
 import { getNamedMessage } from "../../util/error.js";
-import type { AnyFunction } from "../../util/function.js";
+import type { AnyCaller } from "../../util/function.js";
 import type { Identifier, Item, Items, OptionalItem } from "../../util/item.js";
 import type { ItemQuery } from "../../util/query.js";
 import type { Updates } from "../../util/update.js";
-import { type Validators, validateData } from "../../util/validate.js";
 import type { Collection } from "../collection/Collection.js";
 import { ThroughDBProvider } from "./ThroughDBProvider.js";
 
 /** Validate an asynchronous source provider (source can have any type because validation guarantees the type). */
 export class ValidationDBProvider<I extends Identifier = Identifier> extends ThroughDBProvider<I> {
 	override async getItem<T extends Data>(collection: Collection<string, I, T>, id: I): Promise<OptionalItem<I, T>> {
-		return _validateItem(collection.name, await super.getItem(collection, id), collection.id, collection, this.getItem);
+		return _validateItem(collection, await super.getItem(collection, id), this.getItem);
 	}
 
 	override async *getItemSequence<T extends Data>(collection: Collection<string, I, T>, id: I): AsyncIterable<OptionalItem<I, T>> {
-		for await (const item of super.getItemSequence(collection, id))
-			yield _validateItem(collection.name, item, collection.id, collection, this.getItemSequence);
+		for await (const item of super.getItemSequence(collection, id)) yield _validateItem(collection, item, this.getItemSequence);
 	}
 
-	override addItem<T extends Data>(collection: Collection<string, I, T>, data: T): Promise<I> {
-		return super.addItem(collection, validateData(data, collection.props));
+	override async addItem<T extends Data>(collection: Collection<string, I, T>, data: T): Promise<I> {
+		return _validateIdentifier(collection, await super.addItem(collection, collection.validate(data)), this.addItem);
 	}
 
 	override setItem<T extends Data>(collection: Collection<string, I, T>, id: I, data: T): Promise<void> {
-		return super.setItem(collection, id, validateData(data, collection.props));
+		return super.setItem(collection, id, collection.validate(data));
 	}
 
 	override updateItem<T extends Data>(collection: Collection<string, I, T>, id: I, updates: Updates<T>): Promise<void> {
-		return super.updateItem(collection, id, _validateUpdates(collection.name, updates, collection, this.updateItem));
+		return super.updateItem(collection, id, _validateUpdates(collection, updates, this.updateItem));
 	}
 
 	override countQuery<T extends Data>(collection: Collection<string, I, T>, query?: ItemQuery<I, T>): Promise<number> {
@@ -40,15 +37,14 @@ export class ValidationDBProvider<I extends Identifier = Identifier> extends Thr
 	}
 
 	override async getQuery<T extends Data>(collection: Collection<string, I, T>, query?: ItemQuery<I, T>): Promise<Items<I, T>> {
-		return _validateItems(collection.name, await super.getQuery(collection, query), collection.id, collection, this.getQuery);
+		return _validateItems(collection, await super.getQuery(collection, query), this.getQuery);
 	}
 
 	override async *getQuerySequence<T extends Data>(
 		collection: Collection<string, I, T>,
 		query?: ItemQuery<I, T>,
 	): AsyncIterable<Items<I, T>> {
-		for await (const items of super.getQuerySequence(collection, query))
-			yield _validateItems(collection.name, items, collection.id, collection, this.getQuerySequence);
+		for await (const items of super.getQuerySequence(collection, query)) yield _validateItems(collection, items, this.getQuerySequence);
 	}
 
 	override setQuery<T extends Data>(collection: Collection<string, I, T>, query: ItemQuery<I, T>, data: T): Promise<void> {
@@ -56,7 +52,7 @@ export class ValidationDBProvider<I extends Identifier = Identifier> extends Thr
 	}
 
 	override updateQuery<T extends Data>(collection: Collection<string, I, T>, query: ItemQuery<I, T>, updates: Updates<T>): Promise<void> {
-		return super.updateQuery(collection, query, _validateUpdates(collection.name, updates, collection, this.updateQuery));
+		return super.updateQuery(collection, query, _validateUpdates(collection, updates, this.updateQuery));
 	}
 
 	override deleteQuery<T extends Data>(collection: Collection<string, I, T>, query: ItemQuery<I, T>): Promise<void> {
@@ -65,36 +61,46 @@ export class ValidationDBProvider<I extends Identifier = Identifier> extends Thr
 }
 
 /**
- * Validate an entity for a document reference.
+ * Validate a returned `id` for a collection item.
+ */
+function _validateIdentifier<I extends Identifier, T extends Data>(
+	collection: Collection<string, I, T>,
+	id: unknown,
+	caller: AnyCaller,
+): I {
+	try {
+		return collection.id.validate(id);
+	} catch (thrown) {
+		if (typeof thrown !== "string") throw thrown;
+		throw new ValueError(`Invalid identifier for "${collection}"\n${thrown}`, { received: id, caller });
+	}
+}
+
+/**
+ * Validate an entity for a collection item.
  * @throws `ValueError` if one or more items did not validate (conflict because the program is not in an expected state).
  */
 function _validateItem<I extends Identifier, T extends Data>(
-	collection: string, //
+	collection: Collection<string, I, T>, //
 	item: Item<Identifier, Data>,
-	identifier: Schema<I>,
-	schema: DataSchema<T>,
-	caller: AnyFunction,
+	caller: AnyCaller,
 ): Item<I, T>;
 function _validateItem<I extends Identifier, T extends Data>(
-	collection: string,
+	collection: Collection<string, I, T>, //
 	item: OptionalItem<Identifier, Data>,
-	identifier: Schema<I>,
-	schema: DataSchema<T>,
-	caller: AnyFunction,
+	caller: AnyCaller,
 ): OptionalItem<I, T>;
 function _validateItem<I extends Identifier, T extends Data>(
-	collection: string,
+	collection: Collection<string, I, T>, //
 	item: OptionalItem<Identifier, Data>,
-	identifier: Schema<I>,
-	schema: DataSchema<T>,
-	caller: AnyFunction,
+	caller: AnyCaller,
 ): OptionalItem<I, T> {
 	if (!item) return undefined;
 	try {
-		return validateData<Item<I, T>>(item, { id: identifier, ...schema.props } as Validators<Item<I, T>>);
+		return collection.item.validate(item);
 	} catch (thrown) {
 		if (typeof thrown !== "string") throw thrown;
-		throw new ValueError(`Invalid data for "${collection}"\n${thrown}`, { item, caller });
+		throw new ValueError(`Invalid data for "${collection}"\n${thrown}`, { received: item, caller });
 	}
 }
 
@@ -103,39 +109,51 @@ function _validateItem<I extends Identifier, T extends Data>(
  * @throws `ValueError` if one or more items did not validate (conflict because the program is not in an expected state).
  */
 function _validateItems<I extends Identifier, T extends Data>(
-	collection: string,
+	collection: Collection<string, I, T>, //
 	items: Items<Identifier, Data>,
-	identifier: Schema<I>,
-	schema: DataSchema<T>,
-	caller: AnyFunction,
+	caller: AnyCaller,
 ): Items<I, T> {
-	return Array.from(_yieldValidItems(collection, items, { id: identifier, ...schema.props } as Validators<Item<I, T>>, caller));
+	return Array.from(_yieldValidItems(collection, items, caller));
 }
 function* _yieldValidItems<I extends Identifier, T extends Data>(
-	collection: string,
+	collection: Collection<string, I, T>, //
 	items: Items<Identifier, Data>,
-	validators: Validators<Item<I, T>>,
-	caller: AnyFunction,
+	caller: AnyCaller,
 ): Iterable<Item<I, T>> {
 	const messages: MutableArray<string> = [];
 	for (const item of items) {
 		try {
-			yield validateData(item, validators);
+			yield collection.item.validate(item);
 		} catch (thrown) {
 			if (typeof thrown !== "string") throw thrown;
 			messages.push(getNamedMessage(item.id, thrown));
 		}
 	}
-	if (messages.length) throw new ValueError(`Invalid data for "${collection}"\n${messages.join("\n")}`, { items, caller });
+	if (messages.length) throw new ValueError(`Invalid data for "${collection}"\n${messages.join("\n")}`, { received: items, caller });
 }
 
-/** Validate a set of updates for a collection. */
-function _validateUpdates<T extends Data>(collection: string, updates: Updates<T>, schema: DataSchema<T>, caller: AnyFunction): Updates<T>;
-function _validateUpdates<T extends Data>(collection: string, updates: Updates<T>, schema: DataSchema<T>, caller: AnyFunction): Partial<T> {
+/**
+ * Validate a set of updates for a collection.
+ * @throws `ValueError` if the updates do not validate (conflict because the program is not in an expected state).
+ *
+ * @todo This currently is not correct — it's only validating the main keys of the object in a partial way
+ *   We need to make a Schema validator for `Updates<T>` objects.
+ *   And while we're doing it, we should make one for `Query<T>` objects too.
+ */
+function _validateUpdates<I extends Identifier, T extends Data>(
+	collection: Collection<string, I, T>, //
+	updates: Updates<T>,
+	caller: AnyCaller,
+): Updates<T>;
+function _validateUpdates<I extends Identifier, T extends Data>(
+	collection: Collection<string, I, T>, //
+	updates: Updates<T>,
+	caller: AnyCaller,
+): Partial<T> {
 	try {
-		return validateData<Partial<T>>(updates, PARTIAL(schema).props);
+		return PARTIAL(collection).validate(updates);
 	} catch (thrown) {
 		if (typeof thrown !== "string") throw thrown;
-		throw new ValueError(`Invalid updates for "${collection}"\n${thrown}`, { updates, caller });
+		throw new ValueError(`Invalid updates for "${collection}"\n${thrown}`, { received: updates, caller });
 	}
 }
