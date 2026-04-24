d8471036eb5b588a3649f3b4b2b6fc4bb7b2dd0f
Dave Houlbrooke <dave@shax.com>
2026-04-24 23:06:31 +0300

    fix: Add `code` back to `ResponseError`

--- modules/error/ResponseError.ts
+++ modules/error/ResponseError.ts
@@ -10,8 +10,12 @@ export class ResponseError extends Error implements BaseError {
 	/** Provide additional named contextual data that is relevant to the `Error` instance. */
 	readonly [key: string]: unknown;
 
-	constructor(message?: string, options: ResponseErrorOptions = {}) {
+	/** HTTP status code for the response. */
+	readonly code: number;
+
+	constructor(message?: string, { code = 400, ...options }: ResponseErrorOptions = {}) {
 		super(message, options);
+		this.code = code;
 		setBaseErrorOptions(ResponseError, this, options);
 	}
 }
