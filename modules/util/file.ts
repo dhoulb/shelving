818b821315b1e292a247c9ac70078160ce75def7
Dave Houlbrooke <dave@shax.com>
2026-05-13 10:28:44 +0300

    fix: Updates to `splitFileExtension()`

--- modules/util/file.ts
+++ modules/util/file.ts
@@ -1,31 +1,33 @@
 import { RequiredError } from "../error/RequiredError.js";
 import type { AnyCaller } from "./function.js";
+import type { Nullish } from "./null.js";
 
 /** List of file types in `extension: mime` format. */
 export type FileTypes = { [extension: string]: string };
 
 /**
  * Split a filename into its base name and extension in a single pass.
- * - Extension includes the leading dot, e.g. `".ts"`.
+ * - Extension with no leading dot, e.g. `"ts"`.
  * - Returns `undefined` for either part if the filename has no dot or starts with a dot only.
  *
  * @example `splitFileExtension("array.ts")` returns `["array", ".ts"]`
  * @example `splitFileExtension("no-ext")` returns `["no-ext", undefined]`
- * @example `splitFileExtension(".gitignore")` returns `[undefined, ".gitignore"]`
+ * @example `splitFileExtension(".gitignore")` returns `[undefined, "gitignore"]`
+ * @example `splitFileExtension(undefined)` returns `[undefined, undefined]`
  */
-export function splitFileExtension(file = ""): [base: string | undefined, extension: string | undefined] {
-	if (!file.length) return [undefined, undefined];
+export function splitFileExtension(file: Nullish<string> = ""): [base: string | undefined, extension: string | undefined] {
+	if (!file) return [undefined, undefined];
 	const i = file.lastIndexOf(".");
 	if (i < 0) return [file, undefined];
-	return [file.slice(0, i) || undefined, file.slice(i) || undefined];
+	return [file.slice(0, i) || undefined, file.slice(i + 1) || undefined];
 }
 
-/** Get the file extension from a file path, or return `undefined` if the input has no extension. */
-export function getFileExtension(file?: string): string | undefined {
+/** Get the file extension from a file path, e.g. `"md"`, or return `undefined` if the input has no extension. */
+export function getFileExtension(file: Nullish<string>): string | undefined {
 	return splitFileExtension(file)[1];
 }
 
-/** Get the file extension from a file path, or throw `RequiredError` if the input has no extension. */
+/** Get the file extension from a file path e.g. `"tsx"`, or throw `RequiredError` if the input has no extension. */
 export function requireFileExtension(file: string, caller: AnyCaller = requireFileExtension): string {
 	const extension = getFileExtension(file);
 	if (!extension) throw new RequiredError("File extension is required", { received: file, caller });
